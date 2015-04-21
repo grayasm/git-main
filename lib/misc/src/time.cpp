cc736df732e5557da9d479db9634e8bb46fd1b52
Mihai Vasilian <grayasm@gmail.com>
2015-04-21 22:42:13 +0200

    time handling class

--- lib/misc/src/time.cpp
+++ lib/misc/src/time.cpp
@@ -20,19 +20,210 @@
 
 
 #include "time.hpp"
+#include "strconv.hpp"
+#include <limits.h>
+
 
 namespace misc
 {
 	time::time()
 	{
-
+		/* UTC: 1970-Jan-1 00:00:00 */
+		struct tm initm;		
+		initm.tm_year = 70; // 1970-1900
+		initm.tm_mon = 0;
+		initm.tm_mday = 1;
+		initm.tm_hour = 0;
+		initm.tm_min = 0;
+		initm.tm_sec = 0;
+		
+		init( initm );
 	}
+
+	time::time(time_t t)
+	{
+		init(t); 
+	}
+	
+	time::time(int year, enum Month mon, int day, int hour, int min, int sec)
+	{
+		if( year < 1970 || mon < JAN || mon > DEC ||
+			day < 1 || hour < 0 || min < 0 || sec < 0 )
+			throw misc::exception("time is invalid");
+
+		struct tm initm;		
+		initm.tm_year = year - 1900;
+		initm.tm_mon = (mon - JAN);
+		initm.tm_mday = day;
+		initm.tm_hour = hour;
+		initm.tm_min = min;
+		initm.tm_sec = sec;
+		
+		init( initm );
+	}
+	
+	/*	year	Year. 1970 or greater
+	 *  mon		Month. [0-11]
+	 *  day		Day.   [1-31]
+	 *  hour	Hours. [0-23]
+	 *  min		Minutes. [0-59]
+	 *  sec		Seconds. [0-59]
+	 */
+	time::time(int year, int mon, int day, int hour, int min, int sec)
+	{
+		if(year < 1970 || mon < 0 || day < 1 || hour < 0 || min < 0 || sec < 0)
+			throw misc::exception("time is invalid");
+
+		struct tm initm;
+		initm.tm_year = year - 1900;
+		initm.tm_mon =  mon;
+		initm.tm_mday = day;
+		initm.tm_hour = hour;
+		initm.tm_min = min;
+		initm.tm_sec = sec;
+		
+		init( initm );
+	}
+
 	time::time(const time& tc)
 	{
-
+		*this = tc;
 	}
+
+	time& time::operator=(const time& tc)
+	{
+		if(this != &tc)
+		{
+			m_t = tc.m_t;
+			m_tm = tc.m_tm;
+		}
+		return *this;
+	}
+
 	time::~time()
 	{
-
 	}
+
+	time time::operator-(const time& beg) const
+	{
+		double sec = difftime(m_t, beg.m_t);
+		int d=0,h=0,m=0,s=0;
+		reduce(sec, d, h, m, s);
+		time ret(1970, 0, 1+d, 0+h, 0+m, 0+s);
+		return ret;
+	}
+
+	time time::operator+(const time& off) const
+	{
+		time orig(1970, 0, 1, 0, 0, 0);
+		double sec = difftime(off.m_t, orig.m_t);
+		int d=0,h=0,m=0,s=0;
+		reduce(sec, d, h, m, s);
+		time ret(m_tm.tm_year + 1900, m_tm.tm_mon, m_tm.tm_mday + d, 
+				 m_tm.tm_hour + h, m_tm.tm_min + m, m_tm.tm_sec + s);
+		return ret;
+	}
+	
+	bool time::operator<(const time& t) const
+	{
+		double sec = difftime(m_t, t.m_t);
+		return sec < 0;
+	}
+	
+	bool time::operator>(const time& t) const
+	{
+		double sec = difftime(m_t, t.m_t);
+		return sec > 0;
+	}
+
+	bool time::operator==(const time& t) const
+	{
+		double sec = difftime(m_t, t.m_t);
+		return sec == 0;
+	}
+	
+    bool time::operator!=(const time& t) const
+    {
+		double sec = difftime(m_t, t.m_t);
+		return sec != 0;
+	}
+
+	bool time::operator<=(const time& t) const
+	{
+		double sec = difftime(m_t, t.m_t);
+		return sec <= 0;
+	}
+
+	bool time::operator>=(const time& t) const
+	{
+		double sec = difftime(m_t, t.m_t);
+		return sec >= 0;
+	}
+	
+	time& time::operator+=(int sec)
+	{
+		m_tm.tm_sec += sec;
+		init( m_tm );
+		return *this;
+	}
+
+	misc::string time::tostring() const
+	{
+		char  smon[12][4]={ "JAN","FEB","MAR","APR","MAY","JUN",
+							"JUL","AUG","SEP","OCT","NOV","DEC"};
+		misc::string stime;
+		stime += misc::from_value( m_tm.tm_year + 1900 );
+		stime += U("-");
+		stime += smon[m_tm.tm_mon];
+		stime += U("-");
+		stime += misc::from_value( m_tm.tm_mday );
+		stime += U(" ");
+		stime += misc::from_value( m_tm.tm_hour );
+		stime += U(":");
+		stime += misc::from_value( m_tm.tm_min );
+		stime += U(":");
+		stime += misc::from_value( m_tm.tm_sec );
+		return stime;
+	}
+
+	void time::init(time_t t)
+	{
+		/* already epoch time */
+		m_t = t;
+		m_tm = *gmtime(&m_t);
+	}
+	
+	void time::init(struct tm t)
+	{
+		struct tm tmcpy = t;
+		time_t loct = mktime(&tmcpy);	// modify copy of tmval
+		struct tm tmcalc = *localtime(&loct); // calculate tm_yday
+		
+		/* calculate unix time time_t for t */
+		int tm_year = t.tm_year;
+		int tm_yday = tmcalc.tm_yday;
+		int tm_hour = t.tm_hour;
+		int tm_min = t.tm_min;
+		int tm_sec = t.tm_sec;	
+	
+		/* Seconds since the Epoch.
+		 * http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap04.html#tag_04_15
+		 */
+		m_t =
+			tm_sec + tm_min*60 + tm_hour*3600 + tm_yday*86400 +
+			(tm_year-70)*31536000 + ((tm_year-69)/4)*86400 -
+			((tm_year-1)/100)*86400 + ((tm_year+299)/400)*86400;
+
+		/* same time in UTC format */
+		m_tm = *gmtime(&m_t);	
+	}
+	
+	void time::reduce(double sec, int& days, int& hours, int& minutes, int& seconds) const
+	{
+		days = (int)(sec / 86400.);
+		hours = (int)((sec - days * 86400) / 3600.);
+		minutes = (int)((sec - days * 86400 - hours * 3600) / 60.);
+		seconds = (int)(sec - days * 86400 - hours * 3600 - minutes * 60);
+	}
+
 } // namespace
--- lib/misc/src/time.hpp
+++ lib/misc/src/time.hpp
@@ -22,18 +22,85 @@
 
 
 #include <time.h>
+#include "string.hpp"
+
 
 namespace misc
 {
+	/*	The class operates only with time values in UTC.
+	 *	
+	 *	The class retains initial time_t variable which is the number of seconds
+	 *	since Epoch 00:00 Jan 1, 1970 UTC
+	 * 
+	 *	This type is converted at initialization time to a struct tm type which
+	 *	is a calendar date and time broken down into its components.
+	 *	tm values are in UTC/GMT system.
+	 *
+	 */
 	class time
 	{
 	public:
-		time();
-		time(const time& tc);
-		~time();
+		/* Months of the year. */
+		enum Month
+		{
+			JAN=0,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC
+		};
 
+		time();				/* UTC: 1970-JAN-1 00:00:00 */
+		time(time_t t);
+		
+		/*	UTC time format.
+		 *	year	Year. 1970 or greater
+		 *  mon		Month. [JAN-DEC]
+		 *  day		Day.   [1-31]
+		 *  hour	Hours. [0-23]
+		 *  min		Minutes. [0-59]
+		 *  sec		Seconds. [0-59]
+		 */
+		time(int year, enum Month mon, int day, int hour, int min, int sec);
+		
+		time(const time& tc);
+		
+		time& operator=(const time& tc);
+		
+		~time();
+		
+		time operator-(const time& beg) const;		/* req: beg >= this*/
+		time operator+(const time& off) const;
+		bool operator<(const time& t) const;
+		bool operator>(const time& t) const;
+		bool operator==(const time& t) const;
+		bool operator!=(const time& t) const;
+		bool operator<=(const time& t) const;
+		bool operator>=(const time& t) const;
+		
+		time& operator+=(int sec);
+		
+		misc::string tostring() const;
+		
+		static const int daySEC = 86400;
+		static const int hourSEC=  3600;
+		static const int minSEC =    60;
+		
 	private:
-		time_t m_time;
-	};	
+		/*	UTC time format.
+		 *	year	Year. 1970 or greater
+		 *  mon		Month. [0-11]
+		 *  day		Day.   [1-31]
+		 *  hour	Hours. [0-23]
+		 *  min		Minutes. [0-59]
+		 *  sec		Seconds. [0-59]
+		 */
+		time(int year, int mon, int day, int hour, int min, int sec);
+		
+		void init(time_t t);
+		void init(struct tm t);
+		void reduce(double sec, int& days, int& hours, int& minutes, int& seconds) const;
+		
+	private:
+		time_t		m_t;
+		struct tm	m_tm;
+	};
 } // namespace
+
 #endif // __time_hpp__
