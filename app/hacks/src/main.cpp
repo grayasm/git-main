bde8e3eeacff695f19884d622c3254fe2d82b9b2
Mihai Vasilian <grayasm@gmail.com>
2015-04-21 22:45:21 +0200

    added time.cpp to build

--- app/hacks/src/main.cpp
+++ app/hacks/src/main.cpp
@@ -4,15 +4,29 @@
 #include <algorithm>
 #include <assert.h>
 #include <string>
+#include <time.h>
+#include <stdio.h>
+#include "unistd.hpp"
+#include <limits.h>
 #include "string.hpp"
+#include "strconv.hpp"
+#include "time.hpp"
+
 
 
 int main(int, char**)
 {
-	std::string s0;
-	s0.assign(10, '\0');
-	bool test = (s0 == "");
-	bool res = test;
-
+	using namespace misc;
+	setvbuf(stdout, NULL, _IONBF, 0);
+	
+	misc::time t1;
+	misc::time t2(2015, time::FEB, 29, 0, 0, 0);
+	
+	printf("\nt1 %s", t1.tostring().c_str());
+	printf("\nt2 %s", t2.tostring().c_str());
+	
+	t2 += time::hourSEC;
+	printf("\nt3 %s", t2.tostring().c_str());
+	
 	return 0;
 }
