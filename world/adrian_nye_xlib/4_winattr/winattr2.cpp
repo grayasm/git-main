dffa1f653cf060c0323ac1d2311329ce8e044fc6
Mihai Vasilian <grayasm@gmail.com>
2015-07-15 13:46:36 +0200

    n/a

---
+++ world/adrian_nye_xlib/4_winattr/winattr2.cpp
@@ -0,0 +1,86 @@
+/*    Xlib Programming Manual (vol1) by Adrian Nye
+ *    Cap.4 Window Attributes
+ *    Xlib.h/struct XSetWinAttributes
+ *    Xlib.h/extern Window XCreateWindow(..., XSetWindowAttributes*)
+ *    Xlib.h/extern int    XChangeWindowAttributes(..., XSetWindowAttributes*)
+ *
+ *    Because some attributes of top windows are overriden by WM, we will
+ *    create a child window and change only its attributes.
+ */
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+
+
+int main(int argc, char** argv)
+{
+	Display* dpy = XOpenDisplay(NULL);
+	if(!dpy) return -1;
+
+	int scrno = DefaultScreen(dpy);
+	int scrwidth = DisplayWidth(dpy, scrno);
+	int scrheight = DisplayHeight(dpy, scrno);
+
+	int winwidth = scrwidth / 3;
+	int winheight = scrheight / 3;
+	int winborder = 4; //pxl
+
+	Window win = XCreateSimpleWindow(dpy,
+	                                 RootWindow(dpy, scrno),
+	                                 10, 10,
+	                                 winwidth, winheight,
+	                                 winborder,
+	                                 BlackPixel(dpy, scrno),
+	                                 WhitePixel(dpy, scrno));
+	char* wintitle = (char*) "winattr2 - top win + child window";
+	XTextProperty wintitleprop;
+	XStringListToTextProperty(&wintitle, 1, &wintitleprop);
+
+	XSetWMProperties(dpy,
+	                 win,
+	                 &wintitleprop,
+	                 NULL,              // icon property
+	                 argv,
+	                 argc,
+	                 NULL,              // hints about size
+	                 NULL,              // hints about window
+	                 NULL);             // hints about class
+
+	unsigned int childwidth = std::max(25, winwidth/4);
+	unsigned int childheight = std::max(25, winheight/4);
+	unsigned int childborder = 4; // pxl
+	int depth = DefaultDepth(dpy, scrno);
+
+	XSetWindowAttributes childattr;
+	childattr.border_pixel = BlackPixel(dpy, scrno);
+	childattr.background_pixel = WhitePixel(dpy, scrno);
+	childattr.override_redirect = False;
+	unsigned long mask = CWBackPixel | CWBorderPixel | CWOverrideRedirect;
+
+	Window child = XCreateWindow(dpy,
+	                             win,
+	                             20, 20,
+	                             childwidth, childheight,
+	                             childborder,
+	                             depth,
+	                             InputOutput,
+	                             CopyFromParent,
+	                             mask,
+	                             &childattr);
+
+	XMapWindow(dpy, win);
+	XMapWindow(dpy, child);
+
+
+	XEvent event;
+	while(1)
+	{
+		XNextEvent(dpy, &event);
+	}
+
+	return 0;
+}
