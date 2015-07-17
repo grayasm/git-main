97c5983457f09c98c2265dc9a3c224784efe14fe
Mihai Vasilian <grayasm@gmail.com>
2015-07-17 19:17:48 +0300

    n/a

--- world/adrian_nye_xlib/5_GC/gc1.cpp
+++ world/adrian_nye_xlib/5_GC/gc1.cpp
@@ -2,32 +2,33 @@
  *
  *    1. The GC is very much like the window attributes are set.
  *       The structure name is XGCValues and is made up of these symbols:
- *
- *       member
- *       --------------------------------------------------------------
- *       function
- *       plane_mask
- *       foreground
- *       background
- *       line_width
- *       line_style
- *       cap_style
- *       join_style
- *       fill_style
- *       fill_rule
- *       arc_mode
- *       tile
- *       stipple
- *       ts_x_origin
- *       ts_y_origin
- *       font
- *       subwindow_mode
- *       graphics_exposure
- *       clip_x_origin
- *       clip_y_origin
- *       clip_mask
- *       dash_offset
- *       dashes
+
+        member                   mask                       default
+        --------------------------------------------------------------
+        function                 GCFunction                 GXcopy
+        plane_mask               GCPlaneMask                all 1
+        foreground               GCForeground               0
+        background               GCBackground               1
+        line_width               GCLineWidth                0
+        line_style               GCLineStyle                LineSolid
+        cap_style                GCCapStyle                 CapButt
+        join_style               GCJoinStyle                JoinMiter
+        fill_style               GCFillStyle                FillSolid
+        fill_rule                GCFillRule                 EvenOddRule
+        arc_mode                 GCArcMode                  ArcPieSlice
+        tile                     GCTile                     pixmap filled with foreground
+        stipple                  GCStipple                  pixmap filled with 1
+        ts_x_origin              GCTileStipXOrigin          0
+        ts_y_origin              GCTileStipYOrigin          0
+        font                     GCFont                     implementation dependent
+        subwindow_mode           GCSubwindowMode            ClipByChildren
+        graphics_exposure        GCGraphicsExposures        True
+        clip_x_origin            GCClipXOrigin              0
+        clip_y_origin            GCClipYOrigin              0
+        clip_mask                GCClipMask                 None
+        dash_offset              GCDashOffset               0
+        dashes                   GCDashList                 4
+
  */
 
 #include <X11/Xlib.h>
