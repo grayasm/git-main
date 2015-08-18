9df91728945f064e9d1773428694b792bde1562e
Mihai Vasilian <grayasm@gmail.com>
2015-08-18 19:57:21 +0200

    XVisibilityEvent

--- world/adrian_nye_xlib/8_Events/ev11.cpp
+++ world/adrian_nye_xlib/8_Events/ev11.cpp
@@ -1,54 +1,50 @@
 /*
-  ev11.cpp
-  XEvent.xnoexpose is XNoExposeEvent structure.
-
-  Event is sent to notify that an XCopyArea 's source is NOT overlapped.
-  XCopyArea copies from the source window src an area at (x,y,width,height)
-  to the destination window dst at (x,y).
-  In ev9.cpp we tested GraphicsExpose event, which is sent when the source
-  is overlapped resulting in destination being blank (background color).
-  Here ev10.cpp we test NoExpose which simply says that XCopyArea will
-  succeed because source is not overlapped.
-  These 2 events do not tell anything about the dst (destination), but only
-  about the src (source).
+	ev11.cpp
+	XEvent.xvisibility is XVisibilityEvent structure.
+	To receive VisibilityNotify event we must specify VisibilityChangeMask
+	as input selection.
+	This event has 3 states:
+	VisibilityUnobscured - when the window is fully visible; not covered at all;
+	VisibilityPartiallyObscured - window is partially covered by another window;
+	VisibilityFullyObscured - window is totally covered; not visible;
 
 
   typedef union _XEvent {
-          int type;       //must not be changed; first element
-          XAnyEvent xany;
-          XKeyEvent xkey;
-          XButtonEvent xbutton;
-          XMotionEvent xmotion;
-          XCrossingEvent xcrossing;
-          XFocusChangeEvent xfocus;
-          XExposeEvent xexpose;
-          XGraphicsExposeEvent xgraphicsexpose;
-          XNoExposeEvent xnoexpose;      <---------- !!!!!
-          XVisibilityEvent xvisibility;
-          XCreateWindowEvent xcreatewindow;
-          XDestroyWindowEvent xdestroywindow;
-          XUnmapEvent xunmap;
-          XMapEvent xmap;
-          XMapRequestEvent xmaprequest;
-          XReparentEvent xreparent;
-          XConfigureEvent xconfigure;
-          XGravityEvent xgravity;
-          XResizeRequestEvent xresizerequest;
-          XConfigureRequestEvent xconfigurerequest;
-          XCirculateEvent xcirculate;
-          XCirculateRequestEvent xcirculaterequest;
-          XPropertyEvent xproperty;
-          XSelectionClearEvent xselectionclear;
-          XSelectionRequestEvent xselectionrequest;
-          XSelectionEvent xselection;
-          XColormapEvent xcolormap;
-          XClientMessageEvent xclient;
-          XMappingEvent xmapping;
-          XErrorEvent xerror;
-          XKeymapEvent xkeymap;
-          XGenericEvent xgeneric;
-          XGenericEventCookie xcookie;
-          long pad[24];
+			int type;       //must not be changed; first element
+			XAnyEvent xany;
+			XKeyEvent xkey;
+			XButtonEvent xbutton;
+			XMotionEvent xmotion;
+			XCrossingEvent xcrossing;
+			XFocusChangeEvent xfocus;
+			XExposeEvent xexpose;
+			XGraphicsExposeEvent xgraphicsexpose;
+			XNoExposeEvent xnoexpose;
+			XVisibilityEvent xvisibility;       <---------- !!!!!
+			XCreateWindowEvent xcreatewindow;
+			XDestroyWindowEvent xdestroywindow;
+			XUnmapEvent xunmap;
+			XMapEvent xmap;
+			XMapRequestEvent xmaprequest;
+			XReparentEvent xreparent;
+			XConfigureEvent xconfigure;
+			XGravityEvent xgravity;
+			XResizeRequestEvent xresizerequest;
+			XConfigureRequestEvent xconfigurerequest;
+			XCirculateEvent xcirculate;
+			XCirculateRequestEvent xcirculaterequest;
+			XPropertyEvent xproperty;
+			XSelectionClearEvent xselectionclear;
+			XSelectionRequestEvent xselectionrequest;
+			XSelectionEvent xselection;
+			XColormapEvent xcolormap;
+			XClientMessageEvent xclient;
+			XMappingEvent xmapping;
+			XErrorEvent xerror;
+			XKeymapEvent xkeymap;
+			XGenericEvent xgeneric;
+			XGenericEventCookie xcookie;
+			long pad[24];
   } XEvent;
 */
 
@@ -64,7 +60,7 @@
 
 
 
-void printXNoExposeEvent(XNoExposeEvent* xnoexpose);
+void printXVisibilityEvent(XVisibilityEvent* xvisibility);
 
 int main(int argc, char **argv)
 {
@@ -96,7 +92,7 @@ int main(int argc, char **argv)
 	/* X.h
 	   line: 150 to 175   Event definitions;
 	*/
-	long event_mask = ExposureMask | ButtonPressMask;
+	long event_mask = VisibilityChangeMask | ButtonPressMask;
 
 
 
@@ -160,109 +156,22 @@ int main(int argc, char **argv)
 	XMapWindow(dpy, win);
 
 
-
-	// Need an example of XCopyArea (see 6_Draw/draw7.cpp)
-	Pixmap pixmap;
-	unsigned int pix_width, pix_height;
-	int x_hot, y_hot;
-	int error = XReadBitmapFile(dpy,
-	                            win,
-	                            "bitmaps/tile_bitmap",
-	                            &pix_width,
-	                            &pix_height,
-	                            &pixmap,
-	                            &x_hot,
-	                            &y_hot);
-	if(error != BitmapSuccess)
-		return -1;
-
-
-	Pixmap tile = XCreatePixmap(dpy,
-	                            win,
-	                            pix_width,
-	                            pix_height,
-	                            depth);
-
-	XCopyPlane(dpy,
-	           pixmap,  // src - from file
-	           tile,    // dst - created
-	           gc,
-	           0, 0,
-	           pix_width-1, pix_height-1,
-	           0, 0,
-	           1);
-
-	XSetTile(dpy,
-	         gc,
-	         tile);
-
-	XSetFillStyle(dpy,
-	              gc,
-	              FillTiled);
-
-	XSetTSOrigin(dpy,
-	             gc,
-	             0, 0);// x, y
-
-
-
-	bool bcopy = false;
 	XEvent event;
 	while (1)
 	{
 		XNextEvent(dpy, &event);
 
-		// XCopyArea, XClearArea to trigger GraphicsExpose and/or NoExpose
-		if(event.type == Expose &&
-		   event.xexpose.count == 0)
-		{
-			XClearWindow(dpy, win);
-
-			XDrawRectangle(dpy,
-			               win,
-			               gc,
-			               50, 50,
-			               300, 300);
-
-			XFillRectangle(dpy,
-			               win,
-			               gc,
-			               70, 70,
-			               260, 260);
-
-
-			// Testing XCopyArea, XClearArea
-			if(bcopy)
-			{
-				XCopyArea(dpy,
-				          win,
-				          win,
-				          gc,
-				          50, 50,
-				          150, 150,
-				          50, 450);
-				XClearArea(dpy,
-				           win,
-				           50, 50,
-				           150, 150,
-				           False);
-			}
-		}
-
-
 		if(event.type == ButtonPress)
-		{
-			bcopy = !bcopy;
-		}
+			break;
 
 
-		if(event.type == NoExpose)
+		if(event.type == VisibilityNotify)
 		{
-			XNoExposeEvent* xnoexpose = &(event.xnoexpose);
+			XVisibilityEvent* xvisibility = &(event.xvisibility);
 			std::stringstream ss;
-			ss << "XNoExposeEvent";
+			ss << "XVisibilityEvent";
 
-			printXNoExposeEvent(xnoexpose);
+			printXVisibilityEvent(xvisibility);
 			const std::string& msg = ss.str();
 
 			XDrawImageString(dpy,
@@ -282,17 +191,17 @@ int main(int argc, char **argv)
 }
 
 
-void printXNoExposeEvent(XNoExposeEvent* xnoexpose)
+void printXVisibilityEvent(XVisibilityEvent* xvisibility)
 {
-	printf("XNoExposeEvent is XEvent.xnoexpose parameter;\n");
-	printf("xnoexpose.type          = %d (%s)\n",
-	       xnoexpose->type, "XNoExposeEvent");
-	printf("xnoexpose.serial        = %d\n", xnoexpose->serial);
-	printf("xnoexpose.send_event    = %d\n", xnoexpose->send_event);
-	printf("xnoexpose.display       = 0x%x\n", xnoexpose->display);
-	printf("xnoexpose.window        = 0x%x\n", xnoexpose->drawable);
-	printf("xnoexpose.major_code    = %s\n",
-	       (xnoexpose->major_code == X_CopyArea ? "CopyArea" : "CopyPlane"));
-	printf("xnoexpose.minor_code    = %d (%s)\n\n\n",
-	       xnoexpose->minor_code, "not defined in the core");
+	printf("XVisibilityEvent is XEvent.xvisibility parameter;\n");
+	printf("xvisibility.type          = %d (%s)\n",
+	       xvisibility->type, "XVisibilityEvent");
+	printf("xvisibility.serial        = %d\n", xvisibility->serial);
+	printf("xvisibility.send_event    = %d\n", xvisibility->send_event);
+	printf("xvisibility.display       = 0x%x\n", xvisibility->display);
+	printf("xvisibility.window        = 0x%x\n", xvisibility->window);
+	printf("xvisibility.state         = %s\n\n\n",
+		(xvisibility->state == VisibilityUnobscured ? "VisibilityUnobscured" :
+			(xvisibility->state == VisibilityPartiallyObscured ? "VisibilityPartiallyObscured" :
+				"VisibilityFullyObscured")));
 }
