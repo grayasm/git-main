e895f2608e03fd4a186981ce959a1b4da4bd4e39
Mihai Vasilian <grayasm@gmail.com>
2016-02-07 11:04:32 +0100

    n/a

---
+++ world/andrew_krause_gtk+/05_dialogs/assistant.cpp
@@ -0,0 +1,36 @@
+#include <gtk/gtk.h>
+#include <string.h>
+
+void entry_changed (GtkEditable*, GtkAssitant*);
+void button_toggled (GtkCheckButton*, GtkAssitant*);
+void button_clicked (GtkButton*, GtkAssitant*);
+void assitant_cancel (GtkAssitant*, gpointer);
+void assitant_close (GtkAssitant*, gpointer);
+
+
+typedef struct {
+	GtkWidget*      widget;
+	gint            index;
+	const gchar*    title;
+	GtkAssitantPageType type;
+	gboolean        complete;
+} PageInfo;
+
+
+int main (int argc, char** argv)
+{
+	GtkWidget* assitant, *entry, *label, *button, *progress, *hbox;
+	guint i;
+	PageInfo page[5] = {
+		{NULL, -1, "Introduction",           GTK_ASSISTANT_PAGE_INTRO,   TRUE},
+		{NULL, -1, NULL,                     GTK_ASSISTANT_PAGE_CONTENT, FALSE},
+		{NULL, -1, "Click the Check Button", GTK_ASSISTANT_PAGE_CONTENT, FALSE},
+		{NULL, -1, "Click the Button",       GTK_ASSISTANT_PAGE_PROGRESS, FALSE},
+		{NULL, -1, "Confirmation",           GTK_ASSISTANT_PAGE_CONFIRM, TRUE},
+	};
+
+	gtk_init (&argc, &argv);
+
+	/* Create a new assitant widget with no pages. */
+	
+}
