6e8e696e5ff5d85ce1cd870e8bc9ae53ebf590ef
Mihai Vasilian <grayasm@gmail.com>
2016-04-09 14:17:56 +0200

    window

--- world/andrew_krause_gtk+/10_dynamic_user_interfaces/window.cpp
+++ world/andrew_krause_gtk+/10_dynamic_user_interfaces/window.cpp
@@ -1,8 +1,9 @@
 /*
-  1) This source needs .glade file in "Libglade" format, not "GtkBuilder".
+  1) used /usr/bin/glade-3 on centos7 (2016-04-16)
+  2) tell Glade to use format "Libglade" and not "GtkBuilder"
      e.g. <widget class="GtkWindow" id="window">
-  2) .glade Common : Widget/Height request = 50,50 ->minimum window size.
-  3) .glade Signals: had to re-write gtk_main_quit to get it done.
+  3) .glade Common : Widget/Height request = 50,50 ->minimum window size.
+  4) .glade Signals: had to re-write gtk_main_quit to get it done.
  */
 
 #include <gtk/gtk.h>
@@ -12,7 +13,7 @@ int main(int argc, char** argv)
 {
 	gtk_init (&argc, &argv);
 
-	GladeXML* xml = glade_xml_new("01_window.glade", NULL, NULL);
+	GladeXML* xml = glade_xml_new("window.glade", NULL, NULL);
 	GtkWidget* window = glade_xml_get_widget(xml, "window");
 
 	glade_xml_signal_autoconnect (xml);
