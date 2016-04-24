7dc837fc3367f9580b88e24083839aeadb69fbde
Mihai Vasilian <grayasm@gmail.com>
2016-04-24 09:39:19 +0200

    getting the .glade file in shape

--- world/andrew_krause_gtk+/13_all_together/gtk2-styles.cpp
+++ world/andrew_krause_gtk+/13_all_together/gtk2-styles.cpp
@@ -3,6 +3,102 @@
 #include <string.h>
 
 
+/*
+  https://developer.gnome.org/gtk-tutorial/stable/x477.html
+
+GtkObject
+  +GtkWidget
+  | +GtkMisc
+  | | +GtkLabel                   (yes)
+  | | | `GtkAccelLabel
+  | | +GtkArrow
+  | | `GtkImage                   (yes)
+  | +GtkContainer
+  | | +GtkBin
+  | | | +GtkAlignment
+  | | | +GtkFrame                 (yes)
+  | | | | `GtkAspectFrame         (yes)
+  | | | +GtkButton                (yes)
+  | | | | +GtkToggleButton        (yes)
+  | | | | | `GtkCheckButton       (yes)
+  | | | | |   `GtkRadioButton     (yes)
+  | | | | `GtkOptionMenu          (yes)
+  | | | +GtkItem
+  | | | | +GtkMenuItem            (yes)
+  | | | |   +GtkCheckMenuItem     (yes)
+  | | | |   | `GtkRadioMenuItem   (yes)
+  | | | |   +GtkImageMenuItem     (yes)
+  | | | |   +GtkSeparatorMenuItem (yes)
+  | | | |   `GtkTearoffMenuItem   (yes)
+  | | | +GtkWindow                      (yes)
+  | | | | +GtkDialog                    (yes)
+  | | | | | +GtkColorSelectionDialog    (yes)
+  | | | | | +GtkFileSelection           (yes)
+  | | | | | +GtkFontSelectionDialog     (yes)
+  | | | | | +GtkInputDialog             (yes)
+  | | | | | `GtkMessageDialog           (yes)
+  | | | | `GtkPlug
+  | | | +GtkEventBox              (yes)
+  | | | +GtkHandleBox             (yes)
+  | | | +GtkScrolledWindow
+  | | | `GtkViewport
+  | | +GtkBox
+  | | | +GtkButtonBox
+  | | | | +GtkHButtonBox
+  | | | | `GtkVButtonBox
+  | | | +GtkVBox
+  | | | | +GtkColorSelection      (yes)
+  | | | | +GtkFontSelection       (yes)
+  | | | | `GtkGammaCurve          (yes)
+  | | | `GtkHBox
+  | | |   +GtkCombo               (yes)
+  | | |   `GtkStatusbar           (yes)
+  | | +GtkFixed
+  | | +GtkPaned
+  | | | +GtkHPaned
+  | | | `GtkVPaned
+  | | +GtkLayout
+  | | +GtkMenuShell
+  | | | +GtkMenuBar               (yes)
+  | | | `GtkMenu                  (yes)
+  | | +GtkNotebook                (yes)
+  | | +GtkSocket
+  | | +GtkTable
+  | | +GtkTextView
+  | | +GtkToolbar                 (yes)
+  | | `GtkTreeView
+  | +GtkCalendar
+  | +GtkDrawingArea
+  | | `GtkCurve
+  | +GtkEditable
+  | | +GtkEntry
+  | |   `GtkSpinButton
+  | +GtkRuler
+  | | +GtkHRuler
+  | | `GtkVRuler
+  | +GtkRange
+  | | +GtkScale
+  | | | +GtkHScale
+  | | | `GtkVScale
+  | | `GtkScrollbar
+  | |   +GtkHScrollbar
+  | |   `GtkVScrollbar
+  | +GtkSeparator
+  | | +GtkHSeparator
+  | | `GtkVSeparator
+  | +GtkInvisible
+  | +GtkPreview
+  | `GtkProgressBar
+  +GtkAdjustment
+  +GtkCellRenderer
+  | +GtkCellRendererPixbuf
+  | +GtkCellRendererText
+  | +GtkCellRendererToggle
+  +GtkItemFactory
+  +GtkTooltips
+  `GtkTreeViewColumn
+ */
+
 void button1_clicked_cb (GtkButton *bn, gpointer data);
 
 /* Widget states
@@ -29,6 +125,26 @@ struct _GdkColor
   guint16 blue;
 };
 */
+
+/*
+  GdkColor fg[5];
+  GdkColor bg[5];
+  GdkColor light[5];
+  GdkColor dark[5];
+  GdkColor mid[5];
+  GdkColor text[5];
+  GdkColor base[5];
+  GdkColor text_aa[5];      <Halfway between text/base>
+
+  GdkColor black;
+  GdkColor white;
+  PangoFontDescription *font_desc;
+
+  gint xthickness;
+  gint ythickness;
+*/
+
+
 
 
 struct Widgets
