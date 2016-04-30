2da0bd269ce42c940b3abf4c0e1690cd57979875
Mihai Vasilian <grayasm@gmail.com>
2016-04-30 17:50:25 +0200

    new glade file

--- world/andrew_krause_gtk+/13_all_together/gtk2-styles.cpp
+++ world/andrew_krause_gtk+/13_all_together/gtk2-styles.cpp
@@ -1,8 +1,3 @@
-#include <gtk/gtk.h>
-#include <glade/glade.h>
-#include <string.h>
-
-
 /*
   https://developer.gnome.org/gtk-tutorial/stable/x477.html
 
@@ -99,8 +94,6 @@ GtkObject
   `GtkTreeViewColumn
  */
 
-void button1_clicked_cb (GtkButton *bn, gpointer data);
-
 /* Widget states
 typedef enum
 {
@@ -144,82 +137,37 @@ struct _GdkColor
   gint ythickness;
 */
 
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+#include <string.h>
 
 
 
-struct Widgets
-{
-	GtkComboBox* combobox1;
-	GtkColorButton* colorbutton1;
-	GtkButton* button2;
-};
+void on_combobox1_changed (GtkComboBox*, gpointer);
 
 int main (int argc, char** argv)
 {
 	gtk_init (&argc, &argv);
 
-	// gtk_rc_parse ("gtk2-styles.gtkrc");
-
 	GladeXML* xml = glade_xml_new ("gtk2-styles.glade", NULL, NULL);
 	glade_xml_signal_autoconnect (xml);
-	GtkWidget* window = glade_xml_get_widget (xml, "window");
-	Widgets* w = g_slice_new (Widgets);
-	w->combobox1 = GTK_COMBO_BOX (glade_xml_get_widget(xml, "combobox1"));
-	w->colorbutton1 = GTK_COLOR_BUTTON (glade_xml_get_widget(xml, "colorbutton1"));
-	w->button2 = GTK_BUTTON (glade_xml_get_widget(xml, "button2"));
 
-	glade_xml_signal_connect_data (xml,
-	                               "button1_clicked_cb",
-	                               G_CALLBACK(button1_clicked_cb),
-	                               w);
+	GtkWidget* window = glade_xml_get_widget (xml, "window");
+	// GtkWidget* combobox1 = glade_xml_get_widget (xml, "combobox1");
+
+	glade_xml_signal_connect (xml,
+	                          "on_combobox1_changed",
+	                          G_CALLBACK(on_combobox1_changed));
 
 	gtk_widget_show_all (window);
 	gtk_main ();
-
-	g_slice_free(Widgets, w);
 
 	return 0;
 }
 
 
-void button1_clicked_cb (GtkButton *bn, gpointer data)
+void on_combobox1_changed (GtkComboBox* combobox1, gpointer)
 {
-	Widgets* w = (Widgets*)data;
-	GtkComboBox* combobox1 = w->combobox1;
-	GtkColorButton* colorbutton1 = w->colorbutton1;
-	GtkButton* button2 = w->button2;
-
-	const gchar* text = gtk_combo_box_get_active_text (combobox1);
-	GdkColor color;
-	gtk_color_button_get_color (colorbutton1, &color);
-
-
-	g_print ("combobox1=%s\n", text);
-	g_print ("bg color =%d.%d.%d\n",
-		        (int)(color.red * 255. / 65535.0),
-		        (int)(color.green * 255. / 65535.0),
-		        (int)(color.blue * 255. / 65535.0));
-
-	if (text == NULL)
-		return;
-
-	GtkStateType state = GTK_STATE_NORMAL;
-	if ( strcmp(text, "normal") == 0 )
-		state = GTK_STATE_NORMAL;
-	else if (strcmp(text, "active") == 0 )
-		state = GTK_STATE_ACTIVE;
-	else if (strcmp(text, "prelight") == 0 )
-		state = GTK_STATE_PRELIGHT;
-	else if (strcmp(text, "selected") == 0 )
-		state = GTK_STATE_SELECTED;
-	else if (strcmp(text, "insensitive") == 0 )
-		state = GTK_STATE_INSENSITIVE;
-	else
-		return;
-
-	GtkStyle* style = gtk_widget_get_style (GTK_WIDGET(button2));
-	if (style == NULL)
-		return;
-
-	gtk_widget_modify_bg (GTK_WIDGET(button2), state, &color);
+	gchar* text = gtk_combo_box_get_active_text (combobox1);
+	g_print("%s\n", text);
 }
