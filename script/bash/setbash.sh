f559d3de31698a73a03dd62c6aa539285c3ed84a
Mihai Vasilian <grayasm@gmail.com>
2016-05-26 10:39:28 +0200

    bash changes

---
+++ script/bash/setbash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# verbose
+set -x
+
+# check location
+if [ "${PWD##*/}" != "bash" ]; then
+	echo "switch to script directory!"
+	exit
+fi
+
+# copy files
+cp -v bash_aliases  $HOME/.bash_aliases
+cp -v bash_nosave   $HOME/.bash_nosave
+cp -v bashrc        $HOME/.bashrc
