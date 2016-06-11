9d1daa02acc57fe0a338561c88a2d6ed0ca97434
Mihai Vasilian <grayasm@gmail.com>
2016-06-11 18:06:39 +0200

    n/a

--- script/bash/setbash.sh
+++ script/bash/setbash.sh
@@ -10,6 +10,7 @@ if [ "${PWD##*/}" != "bash" ]; then
 fi
 
 # copy files
+cp -v $HOME/.bashrc $HOME/.bashrc.old
 cp -v bash_aliases  $HOME/.bash_aliases
 cp -v bash_nosave   $HOME/.bash_nosave
 cp -v bashrc        $HOME/.bashrc
