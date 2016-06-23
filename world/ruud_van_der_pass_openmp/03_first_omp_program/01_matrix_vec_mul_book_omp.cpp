4b00392f77065c5082c65cb3c97a2af656faadd6
Mihai Vasilian <grayasm@gmail.com>
2016-06-23 04:20:12 +0200

    add omp version

---
+++ world/ruud_van_der_pass_openmp/03_first_omp_program/01_matrix_vec_mul_book_omp.cpp
@@ -0,0 +1,81 @@
+/* Writing the first OpenMP program.
+   Multiplication of matrix B(m,n) with vector c(n).
+   Book example with omp directives.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void mxv(int m, int n, double* a, double* b, double* c);
+void print_mat(int m, int n, double* b);
+void print_vec(int m, double* c);
+
+int main(int argc, char* argv[])
+{
+	double *a,*b,*c;
+	int i,j,m,n;
+
+	printf("Please give m and n: ");
+	scanf("%d %d", &m, &n);
+
+	if ((a=(double *)malloc(m*sizeof(double))) == NULL)
+		perror("memory allocation for a");
+	if ((b=(double *)malloc(m*n*sizeof(double))) == NULL)
+		perror("memory allocation for b");
+	if ((c=(double *)malloc(n*sizeof(double))) == NULL)
+		perror("memory allocation for c");
+
+	printf("Initializing matrix B and vector c\n");
+	for (j=0; j<n; j++)
+		c[j] = 2.0;
+	for (i=0; i<m; i++)
+		for (j=0; j<n; j++)
+			b[i*n+j] = i;
+
+	print_mat (m, n, b);
+	print_vec (n, c);
+
+	printf("Executing mxv function for m = %d n = %d\n", m, n);
+	(void)mxv(m,n,a,b,c);
+
+	print_vec (m, a);
+
+	free(a); free(b);free(c);
+	return(0);
+}
+
+void mxv(int m, int n, double* a, double* b, double* c)
+{
+	int i,j;
+
+#pragma omp parallel for default(none) shared(m,n,a,b,c) private(i,j)
+	for (i=0; i<m; i++)
+	{
+		a[i] = 0.0;
+		for (j=0; j<n; j++)
+			a[i] += b[i*n+j]*c[j];
+	}
+/*-- End of omp parallel for --*/
+}
+
+void print_mat(int m, int n, double* b)
+{
+	int i,j;
+
+	printf("Matrix (%d,%d) = \n", m, n);
+	for (i=0; i<m; i++)
+	{
+		for (j=0; j<n; j++)
+			printf("\t%.2f", b[i*n+j]);
+		printf("\n");
+	}
+}
+
+void print_vec(int m, double* c)
+{
+	int i;
+
+	printf("Vector (%d) = \n", m);
+	for (i=0; i<m; i++)
+		printf("\t%.2f\n", c[i]);
+}
--- world/ruud_van_der_pass_openmp/03_first_omp_program/README.txt
+++ world/ruud_van_der_pass_openmp/03_first_omp_program/README.txt
@@ -1,12 +1,23 @@
 *
   OpenMP pragmas always start with:
-    #pragma omp directive-name [clause [clause ...]] new-line
+    #pragma omp directive-name [clause [[,] clause ...]] new-line
 
   3.2 Matrix Times Vector Operation
   ---------------------------------
   matrix B is the form m x n
   vector c is of length n
 
-  1 2 3   1   1*1 + 2*2 + 3*3   14
-  4 5 6 * 2 = 4*1 + 5*2 + 6*3 = 32
-  7 8 9   3   7*1 + 8*2 + 9*3   50
+    b(m,n) * c(n)= a(m)
+    b(3,3) * c(3)= a(3)
+
+    1 2 3     1     1*1 + 2*2 + 3*3   14
+    4 5 6  *  2  =  4*1 + 5*2 + 6*3 = 32
+    7 8 9     3     7*1 + 8*2 + 9*3   50
+
+  i=m (rows), j=n (columns)
+
+  Since no two dotproducts compute the same element of the result vector and
+  since the order in which the values for the elements a(i) for i = 1,...,m are
+  calculated does not affect correctness of the answer, these computations can
+  be carried out independently. In other words, this problem can be parallelized
+  over the index value i.
