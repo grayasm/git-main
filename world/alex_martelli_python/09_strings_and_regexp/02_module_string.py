0888c5f0b8882d022447e4b80cdcdef49e5aa97b
Mihai Vasilian <grayasm@gmail.com>
2016-07-26 18:55:06 +0200

    string formatting

--- world/alex_martelli_python/09_strings_and_regexp/02_module_string.py
+++ world/alex_martelli_python/09_strings_and_regexp/02_module_string.py
@@ -21,3 +21,19 @@ string.printable
 string.uppercase         # 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 string.whitespace
 # maketrans(from, onto)
+
+# 'maketrans(from, onto) returns a translation table, a plain string of length
+#                        256 that provides mapping from chars. in ascending
+#                        ASCII to another set of characters
+
+identity = string.maketrans('','')
+print 'some string'.translate(identity, 'aeiou')  # sm strng
+
+# the Unicode equivalent would be:
+no_vowels = dict.fromkeys(ord(x) for x in 'aeiou')
+print u'some string'.translate(no_vowels)         # sm strng
+
+# example of turning all other vowels into 'a' and delete 's'
+intoa = string.maketrans('eiou', 'aaaa')
+print 'some string'.translate(intoa)              # sama strang
+print 'some string'.translate(intoa, 's')         # ama trang
