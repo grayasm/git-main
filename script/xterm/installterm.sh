f9913b149efeb8fbe94d7660742fd2c6aec90e8f
Mihai Vasilian <grayasm@gmail.com>
2016-07-31 09:46:28 +0200

    add .Xresources

--- script/xterm/installterm.sh
+++ script/xterm/installterm.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# verbose
+set -x
+
+# loaded when X11 program start, but only if xrdb has not been used in current session
 cp .Xdefaults $HOME/.Xdefaults
 xrdb -merge $HOME/.Xdefaults
+
+# most Display Managers will load this on login
+cp .Xdefaults $HOME/.Xresources
 
 echo "DONE"
