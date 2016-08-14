356d22e7766b61737db982c383e166ec002a4dd4
Mihai Vasilian <grayasm@gmail.com>
2016-08-14 09:41:40 +0200

    add to bash

--- script/bash/setbash.sh
+++ script/bash/setbash.sh
@@ -11,6 +11,9 @@ fi
 
 # copy files
 cp -v $HOME/.bashrc $HOME/.bashrc.old
+cp -v bashrc        $HOME/.bashrc
 cp -v bash_aliases  $HOME/.bash_aliases
 cp -v bash_nosave   $HOME/.bash_nosave
-cp -v bashrc        $HOME/.bashrc
+cp -v bash_i3       $HOME/.bash_i3
+cp -v bash_gpg      $HOME/.bash_gpg
+cp -v bash_iptables $HOME/.bash_iptables
