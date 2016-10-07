7df4c84bd25ffe3f20fd802763801ede889d20e6
Mihai Vasilian <grayasm@gmail.com>
2016-10-07 07:19:56 +0200

    n/a

--- script/fonts/installfonts.sh
+++ script/fonts/installfonts.sh
@@ -1,40 +1,14 @@
 #!/bin/bash
 
-# fonts are usually on another drive (partition)
-if [ ! -d $HOME/1TB/fonts ]; then
-	echo "can not find: $HOME/1TB/fonts"
-	exit
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
 fi
 
-# need fc-cache
-fc-cache --version 2>&1 >/dev/null
-FC_CACHE_IS_AVAILABLE=$?
-if [ $FC_CACHE_IS_AVAILABLE -ne 0 ]; then
-	echo "install fc-cache"
-	exit
+if [ ! -f /etc/yum.repos.d/vasilian.repo ]; then
+	echo "Add vasilian.repo"
+	exit 1
 fi
 
-
-# should be copied in the home directory?
-read -p "Install fonts in $HOME/.fonts [Y/n]=" YN
-case $YN in
-	[Yy] )
-		# don't run as root
-		if [ `id -u` -lt 1000 ]; then
-			echo "log in as regular user"
-			exit
-		fi
-
-		# create destination and copy the files
-		if [ ! -d $HOME/.fonts ]; then
-			mkdir -pv $HOME/.fonts
-		fi
-		cp -v $HOME/1TB/fonts/*  $HOME/.fonts
-		fc-cache $HOME/.fonts
-		echo "Done."
-		exit
-		;;
-	* ) ;; #default
-esac
-
-echo "perhaps later? :)"
+yum --enablerepo=vasilian install win10-fonts
