e023719931a1a6275ac0754450a4472d2e0d3867
Mihai Vasilian <grayasm@gmail.com>
2016-12-03 19:31:56 +0100

    struct

--- tutorial/alex_martelli_python/10_file_and_text_op/05_module_struct.py
+++ tutorial/alex_martelli_python/10_file_and_text_op/05_module_struct.py
@@ -19,3 +19,37 @@ struct.unpack(fmt, string)
 struct.unpack_from(fmt, buffer [,ofset=0])
 struct.calcsize(fmt)
 '''
+
+# The fmt format specifies 3 characters for: byte order, size and alignament.
+'''
+  @    native byte order, native data size and native alignament for the
+       the current platform.
+
+  =    native byte order for the current platform but standard size and
+       alignament
+
+  <    little-endian byte oder (like Intel platforms), standard size and
+       alignament
+
+  >,!  big-endian byte order (network standard); standard size and
+       alignament
+'''
+
+# Also in fmt after the byte specification follows the packing format.
+'''
+  B    unsigned char     int             1 byte
+  b    signed char       int             1 byte
+  c    char              str(length1)    1 byte
+  d    double            float           8 bytes
+  f    float             float           4 bytes
+  H    unsigned short    int             2 bytes
+  h    signed short      int             2 bytes
+  I    unsigned int      long            4 bytes
+  i    signed int        int             4 bytes
+  L    unsigned long     long            4 bytes
+  l    signed long       int             4 bytes
+  P    void*             int             N/A
+  p    char[]            String          N/A
+  s    char[]            String          N/A
+  x    padding byte      no value        1 byte
+'''
