8d04eca66001a432ae6bcba587d7047ede4788c7
Mihai Vasilian <grayasm@gmail.com>
2017-02-11 12:18:23 +0100

    fix makefile

--- tutorial/herbert_schildt_csharp/20_unsafe_code_ptrs_dyntype_miscs/18_test1.cs
+++ tutorial/herbert_schildt_csharp/20_unsafe_code_ptrs_dyntype_miscs/18_test1.cs
@@ -11,3 +11,17 @@ namespace MyNS
         }
     }
 }
+
+
+
+/*
+    Start by compiling test1.cs and test2.cs into DLLs.
+    This can be done easily from the command line by using these commands:
+
+csc /t:library test1.cs
+csc /t:library test2.cs
+
+    Next, compile test3.cs by using this command line:
+
+csc /r:Asm1=test1.dll /r:Asm2=test2.dll test3.cs
+*/
