1ad6e5cc81079965821ef45826a9e2306d49990d
Mihai Vasilian <grayasm@gmail.com>
2017-02-20 08:39:20 +0100

    dumbdb & dbm

--- tutorial/alex_martelli_python/11_persistance_and_databases/05_module_dumbdbm.py
+++ tutorial/alex_martelli_python/11_persistance_and_databases/05_module_dumbdbm.py
@@ -10,3 +10,18 @@
 #    dumbdb    in  dbm.dumb
 #    gdbm      in  dbm.gnu
 #    whichdbm  in  dbm
+
+
+from dbm import dumb
+
+db = dumb.open("dumbdbm", "c")
+db["first"] = "fear"
+db["second"] = "surprise"
+db["third"] = "ruthless efficiency"
+db["fourth"] = "an almost fanatical devotion to the Pope"
+db["fifth"] = "nice red uniforms"
+db.close()
+
+db = dumb.open("dumbdbm", "r")
+for key in db.keys():
+    print(repr(key), repr(db[key]))
