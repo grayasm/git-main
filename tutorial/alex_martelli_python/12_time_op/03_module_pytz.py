2305cd18b2aaef99086b01ad21e179913adf0dbd
Mihai Vasilian <grayasm@gmail.com>
2017-04-24 08:46:44 +0200

    pytz sample

--- tutorial/alex_martelli_python/12_time_op/03_module_pytz.py
+++ tutorial/alex_martelli_python/12_time_op/03_module_pytz.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# before running the script install pytz:
+# pip install pytz
+
+from datetime import datetime
+from datetime import timedelta
+import pytz
+from pytz import timezone
+
+utc = pytz.utc
+print(utc)  # UTC
+
+eastern = timezone('US/Eastern')
+print(eastern.zone)  # US/Eastern
