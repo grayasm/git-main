15fd49b5461e880ae6687145de20d91ddc51940b
Mihai Vasilian <grayasm@gmail.com>
2017-04-30 08:05:25 +0200

    time op

--- tutorial/alex_martelli_python/12_time_op/05_module_sched.py
+++ tutorial/alex_martelli_python/12_time_op/05_module_sched.py
@@ -1,2 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
+# https://docs.python.org/3/library/sched.html
+'''The sched module implements an event-scheduler, letting you easily deal,
+along a single thread of execution, with events that may be scheduled in
+either a “real” or a “simulated” time-scale. sched supplies a scheduler class.
+'''
+
+import sched
+import time
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def print_time(a='default'):
+    print("From print_time", time.time(), a)
+
+
+def print_some_times():
+    print(time.time())
+    s.enter(10, 1, print_time)
+    s.enter(5, 2, print_time, argument=('positional',))
+    s.enter(5, 1, print_time, kwargs={'a': 'keyword'})
+    s.run()
+    print(time.time())
+
+
+print_some_times()
