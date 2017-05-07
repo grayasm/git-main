f932992322f2aad7f76309709310d50bc531933d
Mihai Vasilian <grayasm@gmail.com>
2017-05-07 16:36:19 +0200

    profiling

--- tutorial/alex_martelli_python/18_testing_debugging_optimizing/04_module_unittest.py
+++ tutorial/alex_martelli_python/18_testing_debugging_optimizing/04_module_unittest.py
@@ -1,2 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
+# pag.458
+""" This module tests function reverseWords provided by module mod.py. """
+import unittest
+import mod
+
+
+class ModTest(unittest.TestCase):
+    def testNormalCase(self):
+        self.assertEqual(mod.reverseWords('four score and seven years'),
+                         'years seven and score four')
+
+    def testSingleWord(self):
+        self.assertEqual(mod.reverseWords('justoneword'), 'justoneword')
+
+    def testEmpty(self):
+        self.assertEqual(mod.reverseWords(''), '')
+
+    def testRedundantSpacing(self):
+        self.assertEqual(mod.reverseWords('with spacing redundant'),
+                         'with redundant spacing'),
+
+    def testUnicode(self):
+        self.assertEqual(mod.reverseWords(u'unicode is all right too'),
+                         u'too right all is unicode')
+
+    def testExactly1Argument(self):
+        self.assertRaises(TypeError, mod.reverseWords)
+        self.assertRaises(TypeError, mod.reverseWords, 'one', 'another')
+
+    def testMustBeString(self):
+        self.assertRaises((AttributeError, TypeError), mod.reverseWords, 1)
+
+
+if __name_ == '__main__':
+    unittest.main()
