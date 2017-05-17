abaafce18cf40dc99c09f124b9414d6d5ae160bd
Mihai Vasilian <grayasm@gmail.com>
2017-05-17 07:25:45 +0200

    QString

--- tutorial/johan_thelin_qt/01_the_qt_way_of_cpp/04_using_qstring.cpp
+++ tutorial/johan_thelin_qt/01_the_qt_way_of_cpp/04_using_qstring.cpp
@@ -1,4 +1,14 @@
 /*
+  Another step toward using Qt is to replace any classes from the C++ standard
+  template library (STL) with the corresponding Qt class. Although it is not
+  required (Qt works great alongside the STL), it does make it possible to
+  avoid having to rely on a second framework.
+  The benefit of not using the STL is that you use the same containers, strings,
+  and helpers as Qt does, so the resulting application will most likely be
+  smaller. You also avoid having to track down compatibility issues and strange
+  deviations from the STL standard when moving between platforms and compilers —
+  you can even develop on platforms that do not have implementations of the
+  STL (mobile?)
  */
 
 #include <QString>
