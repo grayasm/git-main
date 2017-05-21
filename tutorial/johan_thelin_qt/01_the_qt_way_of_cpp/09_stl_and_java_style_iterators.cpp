9e516b915e3331dd9d9bf6861934af9a523f0f05
Mihai Vasilian <grayasm@gmail.com>
2017-05-21 14:08:27 +0200

    n/a

--- tutorial/johan_thelin_qt/01_the_qt_way_of_cpp/09_stl_and_java_style_iterators.cpp
+++ tutorial/johan_thelin_qt/01_the_qt_way_of_cpp/09_stl_and_java_style_iterators.cpp
@@ -8,12 +8,12 @@
   QListIterator:        http://doc.qt.io/qt-5/qlistiterator.html
   QMutableListIterator  http://doc.qt.io/qt-5/qmutablelistiterator.html
 
-  qmake-qt5 -project -nopwd $PWD/09_stl_and_java_style_iterators.cpp \
-                        -o project.pro
-  qmake-qt5
-  make
-  ./project
-  git clean -f .
+  $ qmake-qt5 -project -nopwd $PWD/09_stl_and_java_style_iterators.cpp \
+                       -o project.pro
+  $ qmake-qt5
+  $ make
+  $ ./project
+  $ git clean -f .
 
  */
 
