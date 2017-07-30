2b633321d3a8848896a28fb2702d4a19e7269724
Mihai Vasilian <grayasm@gmail.com>
2017-07-30 12:40:10 +0200

    text formatting

---
+++ tutorial/pgsql_community_94doc/01_getting_started/01_access_to_database.sql
@@ -0,0 +1,11 @@
+﻿/*  Chapter 1. Getting Started
+    Run:
+    (a) from pgadmin3 using GUI tool
+    (b) from command line:
+    psql -d mytestdb -U pgmihai
+    \i 01_test.sql
+*/
+
+SELECT version();
+SELECT current_date;
+SELECT 2 + 2;
--- tutorial/pgsql_community_94doc/01_getting_started/01_test.sql
+++
@@ -1,9 +0,0 @@
-﻿/*  Chapter 1. Getting Started
-    Here are more commands, run one of them at will.
-
-SELECT version();
-SELECT current_date;
-SELECT 2 + 2;
-*/
-
-SELECT current_date;
