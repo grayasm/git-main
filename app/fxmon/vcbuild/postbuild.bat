efbdf9811cd5152c383c9294b9b11017f6121380
Mihai Vasilian <grayasm@gmail.com>
2017-08-27 14:45:00 +0200

    clean up
    Signed-off-by: Mihai Vasilian <grayasm@gmail.com>

--- app/fxmon/vcbuild/postbuild.bat
+++
@@ -1,4 +0,0 @@
-SET CONFIG=%1
-echo "Copying ForexConnectAPI to %CONFIG%"
-copy ..\..\..\world\ForexConnectAPI\win32\bin\*.dll   "%CONFIG%"
-copy ..\..\..\world\ForexConnectAPI\win32\samples\cpp\sample_tools\bin\*.dll  "%CONFIG%"
