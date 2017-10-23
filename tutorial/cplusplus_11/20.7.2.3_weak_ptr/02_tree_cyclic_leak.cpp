21e9a052b9e5a27e34e59428fa991955e04dca0d
Mihai Vasilian <grayasm@gmail.com>
2017-10-24 01:11:08 +0200

    unique_ptr

--- tutorial/cplusplus_11/20.7.2.3_weak_ptr/02_tree_cyclic_leak.cpp
+++ tutorial/cplusplus_11/20.7.2.3_weak_ptr/02_tree_cyclic_leak.cpp
@@ -4,7 +4,7 @@
    If we add to the previous binary tree example another small requirement
    that each node will contain a pointer to parent's node, then it will cause
    problems with shared_ptr.
- */
+*/
 
 
 #include <iostream>
@@ -14,28 +14,28 @@ using namespace std;
 
 class Node
 {
-	int value;
+    int value;
 public:
-	std::shared_ptr<Node> left;
-	std::shared_ptr<Node> right;
-	std::shared_ptr<Node> parent;
+    std::shared_ptr<Node> left;
+    std::shared_ptr<Node> right;
+    std::shared_ptr<Node> parent;
 
-	Node(int val) : value(val) { cout << __FUNCTION__ << '\n'; }
-	~Node() { cout << __FUNCTION__<< '\n'; }
+    Node(int val) : value(val) { cout << __FUNCTION__ << '\n'; }
+    ~Node() { cout << __FUNCTION__<< '\n'; }
 };
 
 
 int main()
 {
-	std::shared_ptr<Node> p2 = std::make_shared<Node>(4);
-	p2->left = std::make_shared<Node>(2);
-	p2->left->parent = p2;
-	p2->right = std::make_shared<Node>(5);
-	p2->right->parent = p2;
+    std::shared_ptr<Node> p2 = std::make_shared<Node>(4);
+    p2->left = std::make_shared<Node>(2);
+    p2->left->parent = p2;
+    p2->right = std::make_shared<Node>(5);
+    p2->right->parent = p2;
 
-	cout << "p2 reference count=" << p2.use_count() << '\n';
-	cout << "p2->left reference count=" << p2->left.use_count() << '\n';
-	cout << "p2->right reference count=" << p2->right.use_count() << '\n';
+    cout << "p2 reference count=" << p2.use_count() << '\n';
+    cout << "p2->left reference count=" << p2->left.use_count() << '\n';
+    cout << "p2->right reference count=" << p2->right.use_count() << '\n';
 
-	return 0;
+    return 0;
 }
--- tutorial/cplusplus_11/20.7.2.3_weak_ptr/03_tree_cyclic_ok.cpp
+++ tutorial/cplusplus_11/20.7.2.3_weak_ptr/03_tree_cyclic_ok.cpp
@@ -5,7 +5,7 @@
    we have to use the weak_ptr.
    weak_ptr allows sharing but not owning an object. It's object is created
    by a shared_ptr.
- */
+*/
 
 
 #include <iostream>
@@ -15,31 +15,31 @@ using namespace std;
 
 class Node
 {
-	int value;
+    int value;
 public:
-	std::shared_ptr<Node> left;
-	std::shared_ptr<Node> right;
-	// change the shared_ptr to weak_ptr
-	std::weak_ptr<Node> parent;
+    std::shared_ptr<Node> left;
+    std::shared_ptr<Node> right;
+    // change the shared_ptr to weak_ptr
+    std::weak_ptr<Node> parent;
 
-	Node(int val): value(val) { cout << __FUNCTION__ << '\n'; }
-	~Node() { cout << __FUNCTION__ << '\n'; }
+    Node(int val): value(val) { cout << __FUNCTION__ << '\n'; }
+    ~Node() { cout << __FUNCTION__ << '\n'; }
 };
 
 
 int main()
 {
-	std::shared_ptr<Node> ptr = std::make_shared<Node>(4);
-	ptr->left = std::make_shared<Node>(2);
-	ptr->left->parent = ptr;
-	ptr->right = std::make_shared<Node>(5);
-	ptr->right->parent = ptr;
+    std::shared_ptr<Node> ptr = std::make_shared<Node>(4);
+    ptr->left = std::make_shared<Node>(2);
+    ptr->left->parent = ptr;
+    ptr->right = std::make_shared<Node>(5);
+    ptr->right->parent = ptr;
 
-	std::cout<<"ptr reference count = " << ptr.use_count() << std::endl;
-	std::cout<<"ptr->left reference count = " << ptr->left.use_count() << std::endl;
-	std::cout<<"ptr->right reference count = " << ptr->right.use_count() << std::endl;
-	std::cout<<"ptr->right->parent reference count = " << ptr->right->parent.lock().use_count() << std::endl;
-	std::cout<<"ptr->left->parent reference count = " << ptr->left->parent.lock().use_count() << std::endl;
+    std::cout<<"ptr reference count = " << ptr.use_count() << std::endl;
+    std::cout<<"ptr->left reference count = " << ptr->left.use_count() << std::endl;
+    std::cout<<"ptr->right reference count = " << ptr->right.use_count() << std::endl;
+    std::cout<<"ptr->right->parent reference count = " << ptr->right->parent.lock().use_count() << std::endl;
+    std::cout<<"ptr->left->parent reference count = " << ptr->left->parent.lock().use_count() << std::endl;
 
-	return 0;
+    return 0;
 }
