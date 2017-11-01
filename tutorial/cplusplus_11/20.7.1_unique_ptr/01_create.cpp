56a52f8b65617e2ca50e9e8bc981fa5a5c063d9b
Mihai Vasilian <grayasm@gmail.com>
2017-11-01 10:00:04 +0100

    formatting

--- tutorial/cplusplus_11/20.7.1_unique_ptr/01_create.cpp
+++ tutorial/cplusplus_11/20.7.1_unique_ptr/01_create.cpp
@@ -1,11 +1,15 @@
-/* http://thispointer.com/c11-tutorial/
-   http://thispointer.com/c11-unique_ptr-tutorial-and-examples/
+/* Header     : <memory>
+   Signature  : (1) template<class T, class D = default_delete<T>>
+                    class unique_ptr;
+                (2) template<class T, class D> class unique_ptr<T[], D>
+   Description:
+       -holds a * raw pointer and releases it when goes out of scope
+       -is not copyable
+       -releases the internal * raw pointer on release()
+       -deletes the internal * raw pointer on reset() and becomes empty
 
-   unique_ptr is an object that:
-   -holds a * raw pointer and releases it when goes out of scope
-   -is not copyable
-   -releases the internal * raw pointer on release()
-   -deletes the internal * raw pointer on reset() and becomes empty
+   http://thispointer.com/c11-tutorial/
+   http://thispointer.com/c11-unique_ptr-tutorial-and-examples/
 */
 
 #include <iostream>
