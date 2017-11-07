3074d05d0efe29e0439228fa94425b282dc6bb5a
Mihai Vasilian <grayasm@gmail.com>
2017-11-07 06:00:33 +0100

    packaged_task

--- tutorial/cplusplus_11/30.6_futures/09_packaged_task_with_fcn_pointer.cpp
+++ tutorial/cplusplus_11/30.6_futures/09_packaged_task_with_fcn_pointer.cpp
@@ -1,4 +1,43 @@
+/* Header     : <future>
+   Signature  : template<class> class packaged_task;
+                template<class R, class... Args> class packaged_task<R(Args...)>;
+
+   Description: the class std::packaged_task wraps any callable target
+                e.g. function, lambda expression, bind expression, or another
+                function object, so that it can be invoked asynchronously.
+                Its return value or exception thrown is stored in a shared
+                state which can be accessed through std::future object.
+
+   -----------------------------------------------------------------------------
+   This sample is inspired from:
+   http://thispointer.com/c11-multithreading-part-10-packaged_task-example-and-tutorial
+*/
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <future>
+
+
+std::string read_from_db(std::string token)
+{
+    std::string data = "Data fetched from DB by Filter :: " + token;
+    return data;
+}
+
 int main()
 {
-	return 0;
+    std::packaged_task<std::string(std::string)> task(read_from_db);
+
+    std::future<std::string> result = task.get_future();
+
+    std::thread th(std::move(task), "Arg");
+
+    th.join();
+
+    std::string data = result.get();
+
+    std::cout << data << std::endl;
+
+    return 0;
 }
--- tutorial/cplusplus_11/30.6_futures/11_packaged_task_with_lambda.cpp
+++ tutorial/cplusplus_11/30.6_futures/11_packaged_task_with_lambda.cpp
@@ -1,4 +1,42 @@
+/* Header     : <future>
+   Signature  : template<class> class packaged_task;
+                template<class R, class... Args> class packaged_task<R(Args...)>;
+
+   Description: the class std::packaged_task wraps any callable target
+                e.g. function, lambda expression, bind expression, or another
+                function object, so that it can be invoked asynchronously.
+                Its return value or exception thrown is stored in a shared
+                state which can be accessed through std::future object.
+
+   -----------------------------------------------------------------------------
+   This sample is inspired from:
+   http://thispointer.com/c11-multithreading-part-10-packaged_task-example-and-tutorial
+*/
+
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <future>
+
+
 int main()
 {
-	return 0;
+    std::packaged_task<std::string(std::string)> task(
+        [](std::string token)
+        {
+            std::string data = "Data from " + token;
+            return data;
+        });
+
+    std::future<std::string> result = task.get_future();
+
+    std::thread th(std::move(task), "Arg");
+
+    th.join();
+
+    std::string data = result.get();
+    std::cout << data << std::endl;
+
+    return 0;
 }
