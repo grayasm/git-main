80a0080ae594263042495d78146c35dba82f708b
Mihai Vasilian <grayasm@gmail.com>
2017-12-17 11:51:41 +0100

    add cap.5 samples

---
+++ tutorial/dennis_ritchie_c/05_pointers_and_arrays/ex01_getint.c
@@ -0,0 +1,87 @@
+/*
+ * Filename:    main.c
+ * Author:      Thomas van der Burgt <thomas@thvdburgt.nl>
+ * Date:        01-MAR-2010
+ *
+ * The C Programming Language, second edition,
+ * by Brian Kernighan and Dennis Ritchie
+ *
+ * Exercise 5-1, page 97
+ *
+ * As written, getint treats a + or - not followed by a digit as a valid
+ * representation of zero. Fix it to push such a character back on the
+ * input.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int getint(int *pn);
+int getch(void);
+void ungetch(int c);
+
+
+/* sample implementation of getint */
+int main(void)
+{
+    int i, r;
+
+    r = getint(&i);
+    if (r > 0)
+        printf("%d\n", i);
+    else if (r == 0)
+        printf("not a number\n");
+    else if (r == EOF)
+        printf("end of file\n");
+    else
+        printf("wtf happend?\n");
+    return EXIT_SUCCESS;
+}
+
+/* getint:  get next integer from input into *pn */
+int getint(int *pn)
+{
+    int c, sign;
+
+    while (isspace(c = getch()))   /* skip white space */
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);    /* it's not a number */
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-') {
+        c = getch();
+        if (!isdigit(c)) { /* + or - not followed by a digit */
+            ungetch(c);                      /* ungetch non-digit */
+            ungetch(sign == 1 ? '+' : '-');  /* ungetch + or - */
+            return 0;
+        }
+    }
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn *= sign;
+    if (c != EOF)
+        ungetch(c);
+    return c;
+}
+
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];    /* buffer for ungetch */
+int bufp = 0;         /* next free position in buf */
+
+int getch(void)  /* get a (possibly pushed-back) character */
+{
+   return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)   /* push character back on input */
+{
+   if (bufp >= BUFSIZE)
+       printf("ungetch: too many characters\n");
+   else
+       buf[bufp++] = c;
+}
---
+++ tutorial/dennis_ritchie_c/05_pointers_and_arrays/ex6_str_functions.c
@@ -0,0 +1,137 @@
+/*
+ * Filename:    itoa.c
+ * Author:      Thomas van der Burgt <thomas@thvdburgt.nl>
+ * Date:        04-MAR-2010
+ *
+ * The C Programming Language, second edition,
+ * by Brian Kernighan and Dennis Ritchie
+ *
+ * Exercise 5-6, page 107
+ *
+ * Rewrite appropriate programs from earlier chapters and exercises with
+ * pointers instead of array indexing. Good possibilities include
+ * getline (Chapters 1 and 4), atoi, itoa, and their variants
+ * (Chapters 2, 3, and 4), reverse (Chapter 3), and strindex and getop
+ * (Chapter 4).
+ */
+
+#include <stdio.h>
+#include <stdlib.h>     /* abs() */
+#include <string.h>
+#include <ctype.h>
+
+
+/* strindex:  return index of t in s, -1 if none */
+int strindex(const char *s, const char *t)
+{
+    const char *i, *j, *k;
+
+    for (i = s; *i != '\0'; i++) {      /* for each character in s */
+        j = i, k = t;
+        while(*k != '\0' && *j++ == *k++);
+        if (k-t > 0 && *k == '\0')      /* if it is the start of t */
+            return i-s;                 /* return its index */
+    }
+    return -1;                          /* t was not found return -1 */
+}
+
+/* reverse:  reverse string s in place */
+void reverse(char *s)
+{
+    char tmp, *p;
+    p = s + strlen(s) - 1;
+    while (p > s) {
+        tmp = *s;
+        *s++ = *p;
+        *p-- = tmp;
+    }
+}
+
+/* itoa:  convert n to characters in s */
+void itoa(int n, char *s)
+{
+    int sign;
+    char *p;
+
+    p = s;
+    sign = n;
+    do {        /* generate digits in reverse order */
+        *p++ = abs(n % 10) + '0';   /* get next digit */
+    } while (n /= 10);              /* delete it */
+    if (sign < 0)
+        *p++ = '-';
+    *p = '\0';
+    reverse(s);
+}
+
+#define NUMBER '0'  /* signal that a number was found */
+
+int getch(void);
+void ungetch(int);
+
+/* getop:  get next operator or numeric operand */
+int getop(char *s)
+{
+    char c;
+
+    while ((*s = c = getch()) == ' ' || c == '\t');
+    *++s = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;       /* not a number */
+    --s;                /* remove the \0 */
+    if (isdigit(c))     /* collect intiger part */
+        while (isdigit(*++s = c = getch()));
+    if (c == '.')       /* collect fraction part */
+        while (isdigit(*++s = c = getch()));
+    *s = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];  /* buffer for ungetch */
+int  bufp = 0;      /* next free position in buf */
+
+int getch(void) /* get a (possibly pushed back) character */
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) /* push character back on input */
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+
+/* getline:  get line into s, return length */
+int getline(char *s, int lim)
+{
+    char c, *p;
+
+    p = s;  /* point p to s */
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        *s++ = c;
+    if (c == '\n')
+        *s++ = c;
+    *s = '\0';
+    return s - p;
+}
+
+/* atoi:  convert string s to integer */
+int atoi(const char *s)
+{
+    int n, sign;
+
+    while (isspace(*s))
+        s++;                        /* skip whitespace */
+    sign = (*s == '-') ? -1 : 1;
+    if (*s == '+' || *s == '-')     /* skip sign */
+        s++;
+    for (n = 0; isdigit(*s); s++)
+        n = 10 * n + (*s -'0');
+    return sign * n;
+}
