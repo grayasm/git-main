391bd3fd34739ee01d11d368f6a6dd1b36391304
Mihai Vasilian <grayasm@gmail.com>
2018-01-31 09:26:53 +0100

    Add ResponseListener4MarketOrders and rename ResponseListener4EntryOrders;
    Signed-off-by: Mihai Vasilian <grayasm@gmail.com>

---
+++ lib/fxcmAdapter/src/ResponseListener4MarketOrders.cpp
@@ -0,0 +1,119 @@
+/*
+	Copyright (C) 2017 Mihai Vasilian
+
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the
+	Free Software Foundation; either version 2 of the License, or (at your
+	option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program. If not, see http://www.gnu.org/licenses/.
+
+	contact: grayasm@gmail.com
+*/
+
+#include "ResponseListener4Orders.hpp"
+#include "autocritical_section.hpp"
+#include "stream.hpp"
+#include <sstream>
+
+
+
+
+namespace fxcm
+{
+	ResponseListener4Orders::ResponseListener4Orders(IO2GSession* session)
+	{
+		m_Session = session;
+		m_Session->addRef();
+		m_RefCount = 1;
+		// m_ResponseEvent is non-signaled
+		m_RequestID = "";
+		m_Response = NULL;
+		// m_CriticalSection - is unlocked
+	}
+
+	long ResponseListener4Orders::addRef()
+	{
+		misc::autocritical_section autocs(m_CriticalSection);
+		m_RefCount++;
+		return m_RefCount;
+	}
+
+	long ResponseListener4Orders::release()
+	{
+		// protect m_CriticalSection against 'delete this'
+		{
+			misc::autocritical_section autocs(m_CriticalSection);
+			m_RefCount--;
+			if (m_RefCount)
+				return m_RefCount;
+		}
+
+		delete this;
+		return 0;
+	}
+
+	void ResponseListener4Orders::onRequestCompleted(const char* requestId, IO2GResponse* response)
+	{
+		if (response && m_RequestID == requestId)
+		{
+			m_Response = response;
+			m_Response->addRef();
+			if (response->getType() != CreateOrderResponse)
+				m_ResponseEvent.unlock();
+		}
+	}
+
+	void ResponseListener4Orders::onRequestFailed(const char* requestId, const char* error)
+	{
+		if (m_RequestID == requestId)
+		{
+			misc::cout << __FUNCTION__ 
+				<< ": The request has been failed. ID: "
+				<< requestId << " : " << error << std::endl;
+			m_ResponseEvent.unlock();
+		}
+	}
+
+	void ResponseListener4Orders::onTablesUpdates(IO2GResponse* tablesUpdates)
+	{
+	}
+
+	void ResponseListener4Orders::SetRequestID(const misc::string& requestID)
+	{
+		m_RequestID = requestID;
+		if (m_Response)
+		{
+			m_Response->release();
+			m_Response = NULL;
+		}
+		m_ResponseEvent.unlock();
+	}
+
+	bool ResponseListener4Orders::WaitEvents()
+	{
+		return (m_ResponseEvent.trylock(30000) == 0);
+	}
+
+	IO2GResponse* ResponseListener4Orders::GetResponse()
+	{
+		if (m_Response)
+			m_Response->addRef();
+		return m_Response; // ~O2G2Ptr will release() it.
+	}
+
+	ResponseListener4Orders::~ResponseListener4Orders()
+	{
+		if (m_Response)
+			m_Response->release();
+		m_Session->release();
+		// m_ResponseEvent will CloseHandle itself on ~dtor
+	}
+
+} // namespace
---
+++ lib/fxcmAdapter/src/ResponseListener4MarketOrders.hpp
@@ -0,0 +1,67 @@
+/*
+Copyright (C) 2017 Mihai Vasilian
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+contact: grayasm@gmail.com
+*/
+
+
+#ifndef __ResponseListener4Orders_hpp__
+#define __ResponseListener4Orders_hpp__
+
+#include <stddef.h>
+#include <ForexConnect.h>
+#include "string.hpp"
+#include "event.hpp"
+#include "critical_section.hpp"
+
+
+
+namespace fxcm
+{
+	class ResponseListener4Orders : public IO2GResponseListener
+	{
+	public:
+		ResponseListener4Orders(IO2GSession* session);
+
+		// vtable begin
+		long addRef();
+		long release();
+		void onRequestCompleted(const char* requestId, IO2GResponse* response=0);
+		void onRequestFailed(const char* requestId, const char* error);
+		void onTablesUpdates(IO2GResponse* tablesUpdates);
+		// vtable end
+
+		void SetRequestID(const misc::string& requestID);
+		bool WaitEvents();
+		IO2GResponse* GetResponse();
+
+
+	protected:
+		~ResponseListener4Orders();
+
+	private:
+		long					m_RefCount;
+		IO2GSession*			m_Session;
+		misc::string			m_RequestID;
+		misc::event				m_ResponseEvent;
+		IO2GResponse*			m_Response;
+		misc::critical_section	m_CriticalSection;
+	};
+} // namespace
+
+
+#endif // __ResponseListener4Orders_hpp__
