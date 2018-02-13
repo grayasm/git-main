b0d8d408922156a8e867ee95b9f3248f49674ae6
Mihai Vasilian <grayasm@gmail.com>
2018-02-13 02:07:46 +0100

    fix: closing amount & logging trades.
    Signed-off-by: Mihai Vasilian <grayasm@gmail.com>

--- app/fxmon/src/RunTransaction.cpp
+++ app/fxmon/src/RunTransaction.cpp
@@ -26,10 +26,26 @@
 #include "Transaction.hpp"
 
 
-void ClosePosition(fxcm::Session& session, const fxcm::Offer& offer, const fx::Transaction& trIn);
-void OpenPosition(fxcm::Session& session, const fxcm::Offer& offer, int lots, bool buy, fx::Transaction& result);
+void OpenPosition(	const fxcm::IniParams& iniParams,
+					fxcm::Session& session,
+					const fxcm::Offer& offer,
+					int lots,
+					bool buy,
+					fx::Transaction& result);
 
-static const misc::string tradingLog("/tmp/TradingResult.txt");
+void ClosePosition(	const fxcm::IniParams& iniParams,
+					fxcm::Session& session,
+					const fxcm::Offer& offer,
+					const fx::Transaction& trIn,
+					fx::Transaction& trOut); // return closed transaction
+
+// removed from result the closed positions
+void RemoveClosedPositions(	fx::Transaction& trResult,
+							const fx::Transaction& trClosed);
+
+void Log(const fxcm::IniParams& iniParams,
+		 const misc::string& message);
+
 
 void RunTransaction()
 {
@@ -50,9 +66,10 @@ void RunTransaction()
 	fx::Price price;
 	fx::Transaction tr;
 	double totalPL = 0;
-	double renkoPL = 15; // renko size
-	int lotsK = 1;
+	double renkoPL = 2; // renko size
+	int lots = 1;
 	bool buy = true;
+
 
 	//  Frankfurt: Xetra trading takes place from 9.00a.m. until 5.30 p.m. CET. (UTC+100)
 	//  New York:  Core Trading Session: 9:30 AM TO 4:00 PM ET (UTC-500)
@@ -110,7 +127,7 @@ void RunTransaction()
 			break; // error with the price feed
 
 		// when can I open a position
-		bool canOpen = true; // for now; (tnow.hour_() >= hopen && tnow.hour_() <= hclose);
+		bool canOpen = true; // (tnow.hour_() >= hopen && tnow.hour_() <= hclose);
 		if (!canOpen && tr.IsEmpty())
 		{
 			msleep(1000 * misc::time::minSEC); // slow down a bit
@@ -133,12 +150,12 @@ void RunTransaction()
 			if (pips > renkoPL && canOpen)
 			{
 				buy = true;
-				OpenPosition(session, offer, lotsK, buy, tr);
+				OpenPosition(*iniParams, session, offer, lots, buy, tr);
 			}
 			else if (pips < -renkoPL && canOpen)
 			{
 				buy = false;
-				OpenPosition(session, offer, lotsK, buy, tr);
+				OpenPosition(*iniParams, session, offer, lots, buy, tr);
 			}				
 			else
 				continue;
@@ -162,15 +179,19 @@ void RunTransaction()
 
 		if (diffPL < -2 * renkoPL)
 		{
-			ClosePosition(session, offer, tr);
+			fx::Transaction result;
+			ClosePosition(*iniParams, session, offer, tr, result);
+			RemoveClosedPositions(tr, result);
 			
-			// TODO: must detect if all positions were closed correctly
-
 			closedPL += curPL;
 			closedGPL += curGPL;
 
 			misc::cout << "curPL=" << curPL << " closedGPL=" << closedGPL
 				<< std::endl;
+
+
+			if (!tr.IsEmpty())
+				continue; // error during ClosePosition!
 
 			// reset initialOffer at the end of the day
 			if (!canOpen)
@@ -182,7 +203,7 @@ void RunTransaction()
 			else
 			{
 				buy = !buy;
-				OpenPosition(session, offer, lotsK, buy, tr);
+				OpenPosition(*iniParams, session, offer, lots, buy, tr);
 				totalPL = 0;
 			}
 		}
@@ -192,69 +213,111 @@ void RunTransaction()
 }
 
 
-void OpenPosition(fxcm::Session& session, const fxcm::Offer& offer, int lots, bool buy, fx::Transaction& result)
+void OpenPosition(	const fxcm::IniParams& iniParams,
+					fxcm::Session& session,
+					const fxcm::Offer& offer,
+					int lots,
+					bool buy,
+					fx::Transaction& result)
 {
 	misc::vector<fx::Position> openPositions;
-	session.OpenPosition(offer, lots, buy, openPositions);
+	int ret = session.OpenPosition(offer, lots, buy, openPositions);
+	if (ret != fxcm::ErrorCodes::ERR_SUCCESS)
+	{
+		Log(iniParams, "session.OpenPosition(..) failed.\n");
+		return;
+	}
+		
+	misc::string message(offer.GetTime().tostring());
+	message += " => Open Position: \n";
+	result = fx::Transaction(); // clear the result
 
-	result = fx::Transaction(); // clear
 	for (size_t i = 0; i < openPositions.size(); ++i)
 	{
-		result.Add(openPositions[i]);
+		const fx::Position& pos = openPositions[i];
+		result.Add(pos);
+		message += pos.ToString();
+		message += "\n";
 	}
 
-
-	FILE *pf = fopen(tradingLog.c_str(), "a+");
-	if (pf == NULL)
-		return;
-
-	misc::string slog;
-	slog += offer.GetTime().tostring();
-	slog += (buy == true ? ", B:" : ", S:");
-	slog += (buy == true ? misc::from_value(offer.GetAsk(), 5) :
-		misc::from_value(offer.GetBid(), 5));
-	slog += ", L(k)=";
-	slog += misc::from_value(lots);
-	slog += "\n";
-
-	fwrite(slog.c_str(), sizeof(char), slog.size(), pf);
-	fclose(pf);
+	Log(iniParams, message);
 }
 
-void ClosePosition(fxcm::Session& session, const fxcm::Offer& offer, const fx::Transaction& trIn)
+void ClosePosition(	const fxcm::IniParams& iniParams,
+					fxcm::Session& session,
+					const fxcm::Offer& offer,
+					const fx::Transaction& trIn,
+					fx::Transaction& trOut) // return closed transaction
 {
-	misc::vector<fx::Position> result;
-	bool buy = false;
+	misc::vector<fx::Position> posOut;
+	const fx::Transaction::Positions& posIn = trIn.GetPositions();
+	trOut = fx::Transaction(); // clear result
+	bool bFail = false;
 
-	const fx::Transaction::Positions& positions = trIn.GetPositions();
-	for (size_t i = 0; i < positions.size(); ++i)
+	misc::string message(offer.GetTime().tostring());
+	message += " => Close Position: \n";
+	
+	for (size_t i = 0; i < posIn.size(); ++i)
 	{
-		const fx::Position& position = positions[i];
-		session.ClosePosition(offer, position, result);
-		
-		if (i == 0)
-			buy = position.IsBuy();
+		const fx::Position& pos = posIn[i];
+		int ret = session.ClosePosition(offer, pos, posOut);
+
+		if (ret != fxcm::ErrorCodes::ERR_SUCCESS)
+			bFail = true;
+
+		for (size_t j = 0; j < posOut.size(); ++j)
+		{
+			const fx::Position& pos2 = posOut[j];
+			trOut.Add(pos2);
+			message += pos2.ToString();
+			message += "\n";
+		}		
 	} // for
 
-	
-	FILE *pf = fopen(tradingLog.c_str(), "a+");
+	fx::Price price(offer.GetAsk(), offer.GetBid());
+	message += "PL=";
+	message += misc::from_value(trOut.GetPL(offer.GetInstrument(), price), 2);
+	message += " GPL=";
+	message += misc::from_value(trOut.GetGPL(offer.GetInstrument(), price), 2);
+	message += "\n";
+
+	Log(iniParams, message);
+
+	if (bFail)
+		Log(iniParams, "session.ClosePosition(..) failed.\n");
+}
+
+void RemoveClosedPositions(	fx::Transaction& trResult,
+							const fx::Transaction& trClosed)
+{
+	fx::Transaction::Positions& dest =
+		const_cast<fx::Transaction::Positions&>(trResult.GetPositions());
+	const fx::Transaction::Positions& closedVec = trClosed.GetPositions();
+	for (size_t i = 0; i < closedVec.size(); ++i)
+	{
+		const fx::Position& pos2rem = closedVec[i];
+		for (size_t j = 0; j < dest.size(); ++j)
+		{
+			if (pos2rem.GetTradeID() == dest[j].GetTradeID())
+			{
+				dest.erase(dest.begin() + j);
+				break;
+			}
+		}
+	}
+}
+
+
+void Log(const fxcm::IniParams& iniParams,
+		 const misc::string& message)
+{
+	if (!iniParams.GetEnableLogging())
+		return;
+
+	FILE *pf = fopen(iniParams.GetLoggingFile().c_str(), "a+");
 	if (pf == NULL)
 		return;
 
-
-	misc::string slog;
-	slog += offer.GetTime().tostring();
-	slog += (buy == true ? ", S:" : ", B:");
-	slog += (buy == true ? misc::from_value(offer.GetBid(), 5) :
-		misc::from_value(offer.GetAsk(), 5));
-	
-	slog += ", PL(1k)=";
-	fx::Price price(offer.GetAsk(), offer.GetBid());
-	slog += misc::from_value(trIn.GetPL(offer.GetInstrument(), price), 2);
-	slog += ", GPL=";
-	slog += misc::from_value(trIn.GetPL(offer.GetInstrument(), price), 2);
-	slog += "\n";
-
-	fwrite(slog.c_str(), sizeof(char), slog.size(), pf);
+	fwrite(message.c_str(), sizeof(char), message.size(), pf);
 	fclose(pf);
 }
