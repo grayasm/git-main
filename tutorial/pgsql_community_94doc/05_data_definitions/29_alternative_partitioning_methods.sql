ff7e2554cf103776b6b2f03417e6224546b08e19
Mihai Vasilian <grayasm@gmail.com>
2018-08-04 12:13:32 +0200

    partitioning

---
+++ tutorial/pgsql_community_94doc/05_data_definitions/29_alternative_partitioning_methods.sql
@@ -0,0 +1,7 @@
+/*  Chapter 5. Data Definition
+    5.9. Partitioning
+    5.9.5. Alternative partitioning methods
+
+    psql -d mytestdb -U pgmihai
+    \i 29_alternative_partitioning_methods.sql
+*/
