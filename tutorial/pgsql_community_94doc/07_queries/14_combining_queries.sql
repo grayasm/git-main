d1e39d317bb0b8d16ee5f3a7f9a3bfb7714fc636
Mihai Vasilian <grayasm@gmail.com>
2018-09-07 18:31:56 +0200

    fix

--- tutorial/pgsql_community_94doc/07_queries/14_combining_queries.sql
+++ tutorial/pgsql_community_94doc/07_queries/14_combining_queries.sql
@@ -19,45 +19,57 @@
 DROP TABLE IF EXISTS sales1, sales2 CASCADE;
 CREATE TABLE sales1 (nume TEXT, ammount NUMERIC);
 CREATE TABLE sales2 (nume TEXT, ammount NUMERIC);
-INSERT INTO sales1 VALUES ('Mike', 1000), ('John', 1100), ('Mary', 1200);
-INSERT INTO sales1 VALUES ('Mike', 1100), ('John', 1200), ('Mary', 1300);
+INSERT INTO sales1 VALUES ('Mike', 100), ('John', 200), ('Mary', 300);
+INSERT INTO sales1 VALUES ('Mike', 101), ('John', 201), ('Mary', 300);
+
+
+
+
+/*      We often use the UNION operator to combine data from similar tables
+        that are not perfectly normalized.
+        Those tables are often found in the reporting or data warehouse system.
+        UNION - will remove duplicate rows
+        UNION ALL - will include duplicate rows
+*/
+
+
 
 SELECT * FROM sales1 UNION SELECT * FROM sales2;
 /*
- nume | ammount 
+ nume | ammount
 ------+---------
- John |    1100
- Mary |    1200
- Mike |    1100
- Mary |    1300
- John |    1200
- Mike |    1000
-(6 rows)
+ John |     201
+ Mike |     101
+ John |     200
+ Mary |     300
+ Mike |     100
+(5 rows)
 */
 
 SELECT * FROM sales1 UNION ALL SELECT * FROM sales2;
 /*
- nume | ammount 
+ nume | ammount
 ------+---------
- Mike |    1000
- John |    1100
- Mary |    1200
- Mike |    1100
- John |    1200
- Mary |    1300
+ Mike |     100
+ John |     200
+ Mary |     300
+ Mike |     101
+ John |     201
+ Mary |     300
 (6 rows)
 */
 
 SELECT * FROM sales1 UNION ALL SELECT * FROM sales2 ORDER BY nume ASC, ammount DESC;
 /*
- nume | ammount 
+ nume | ammount
 ------+---------
- John |    1200
- John |    1100
- Mary |    1300
- Mary |    1200
- Mike |    1100
- Mike |    1000
+ John |     201
+ John |     200
+ Mary |     300
+ Mary |     300
+ Mike |     101
+ Mike |     100
 (6 rows)
 */
+
 
