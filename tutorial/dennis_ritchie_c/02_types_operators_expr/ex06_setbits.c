abe7c0b671106ee9fed41ef497060d1668a4a121
Mihai Vasilian <grayasm@gmail.com>
2018-09-19 02:28:22 +0200

    K&R ex:2-6 setbits

--- tutorial/dennis_ritchie_c/02_types_operators_expr/ex06_setbits.c
+++ tutorial/dennis_ritchie_c/02_types_operators_expr/ex06_setbits.c
@@ -8,24 +8,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned setbits(unsigned x, int p, int n, int y);
+unsigned setbits(unsigned x, unsigned p, unsigned n, unsigned y);
+unsigned setbits2(unsigned x, unsigned p, unsigned n, unsigned y);
 
 int main(void)
 {
-    printf("%u\n", setbits(170, 4, 3, 7));
+    /*
+      p = 4
+      n = 3
+      y = 0000 0011    =3    <-take these 3 bits
+                ^^^
+      x = 1111 0101    =245  <-set them at pos 4
+           ^^^
+      s = 1011 0101    =181  <-leave the others unchanged
+           ^^^
+     */
+    printf("check: 181=%u\n", setbits(245, 4, 3, 3));
+    printf("check: 181=%u\n", setbits2(245, 4, 3, 3));
+
     return EXIT_SUCCESS;
 }
 
-/* getbits:  place n rightmost bits from position p at position y */
-unsigned setbits(unsigned x, int p, int n, int y)
+
+unsigned setbits(unsigned x, unsigned p, unsigned n, unsigned y)
 {
-    int i, mask, j;
-    /* The rightmost n bits of y. */
-    i = (x >> (y+1-n)) & ~(~0 << n);
-    /* A mask with zeros at the rightmost n bits of p. */
-    mask = ~(((1 << n)-1) << (p+1-n));
-    /* Make rightmost n bits of p zero ... */
-    j = mask & x;
-    /* ... and replace them with the rightmost n bits of y. */
-    return j | i << (p+1-n);
+    unsigned a,b,c,d,m;
+    unsigned f,g,i,j;
+
+    
+    a = ~(~(unsigned)0 << n); /* 0000 0111 */
+    b = ~a;                   /* 1111 1000 */
+    
+    c = (a << p);             /* 0111 0000 */
+    d = ~(~b << p);           /* 1000 1111 */
+
+    m = y & a;                /* 0000 0011 */
+    m = (m << p);             /* 0011 0000 */
+
+    f = x & c;                /* 0111 0000 */
+    g = x & d;                /* 1000 0101 */
+
+    i = f & m;                /* 0011 0000 */
+    j = i | g;                /* 1011 0101 */
+
+    return j;
+}
+
+unsigned setbits2(unsigned x, unsigned p, unsigned n, unsigned y)
+{
+    unsigned m1,m2,m3;
+    m1 = y & ~(~(unsigned)0 << n);        /* 0000 0011 */
+    m1 = (m1 << p);                       /* 0011 0000 */
+    m2 = ~(~(unsigned)0 << n) << p;       /* 0111 0000 */
+    m3 = ~m2;                             /* 1000 1111 */
+    return ((x & m1) | (x & m3));         /* 1011 0101 */
 }
