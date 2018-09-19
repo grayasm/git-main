6c6f7b5953ad6fbb87cdd2e703803ee232b984ad
Mihai Vasilian <grayasm@gmail.com>
2018-09-19 21:41:47 +0200

    improve ex. text in the header

--- tutorial/dennis_ritchie_c/02_types_operators_expr/ex07_invert.c
+++ tutorial/dennis_ritchie_c/02_types_operators_expr/ex07_invert.c
@@ -1,8 +1,8 @@
 /* Cap.2 Types, Operators and Expressions, pag.49
 
-   Exercise 2-7.  Write a function invert(x,p,n) that returns x with the n bits
-   that begin at position p inverted (i.e. 1 changed into 0 and vice versa)
-   leaving the other unchanged.
+   Exercise 2-7: Write a function invert(x,p,n) that returns x with the n bits
+                 that begin at position p inverted, i.e. 1 changed into 0
+                 and vice versa, leaving the other unchanged.
 */
 
 #include <stdio.h>
