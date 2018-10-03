4133ef1003507873c0a71ec922222a48eadc45cc
Mihai Vasilian <grayasm@gmail.com>
2018-10-03 15:40:22 +0200

    review cap.6 examples

---
+++ tutorial/dennis_ritchie_c/06_structures/ex6-01_getword_improved.c
@@ -0,0 +1,6 @@
+/* Cap.6 Structures, pag.136
+
+   Exercise 6-1: Our version of getword does not properly handle underscores,
+                 string constants, comments, or preprocessor control lines.
+                 Write a better version.
+*/
