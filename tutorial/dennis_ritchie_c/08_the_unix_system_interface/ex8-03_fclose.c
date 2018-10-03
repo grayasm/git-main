09c8737be25ae87e2e0a320ce2d928cb6fa03b0b
Mihai Vasilian <grayasm@gmail.com>
2018-10-03 16:34:38 +0200

    review cap.8 examples

---
+++ tutorial/dennis_ritchie_c/08_the_unix_system_interface/ex8-03_fclose.c
@@ -0,0 +1,4 @@
+/* Cap.8 The Unix System Interface, pag.179
+
+   Exercise 8-3: Design and write _flushbuf, fflush and fclose.
+*/
---
+++ tutorial/dennis_ritchie_c/08_the_unix_system_interface/ex8-08_bfree.c
@@ -0,0 +1,8 @@
+/* Cap.8 The Unix System Interface, pag.189
+
+   Exercise 8-8: Write a routine bfree(p,n) that will free an arbitrary block p
+                 of n characters into the free list maintained by malloc and
+                 free. By using bfree, a user can add a static or external array
+                 to the free list at any time.
+*/
+
