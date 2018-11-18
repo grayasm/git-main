8aa63c80a0f2f7e697d64fa363dbae4758341b2b
Mihai Vasilian <grayasm@gmail.com>
2018-11-18 17:47:32 +0100

    add cap.7 samples

--- tutorial/dennis_ritchie_c/07_input_and_output/ex7-02_nongraphic.c
+++ tutorial/dennis_ritchie_c/07_input_and_output/ex7-02_nongraphic.c
@@ -4,5 +4,54 @@
                  way. As a minimum, it should print non-graphic characters in
                  octal or hexadecimal according to local custom, and break long
                  text lines.
+
+http://www.learntosolveit.com/cprogramming/Ex_7.2_nongraphic.html
 */
 
+
+#include<stdio.h>
+#include<ctype.h>
+
+#define MAXLINE 100 /* maximum number of chars in one line */
+#define OCTLEN  6   /* length of an octal value */
+
+/* print arbitrary input in a sensible way */
+
+int main(void)
+{
+    int c,pos;
+    int inc(int pos,int n);
+
+    pos = 0;        /* position in the line */
+
+    while((c=getchar()) != EOF)
+        if(iscntrl(c) || c == ' ')
+        {
+            /* non-graphic or blank character */
+            pos = inc(pos,OCTLEN);
+            printf("\\%03o",c);
+            /* newline character */
+            if(c == '\n')
+                pos = 0;
+            putchar('\n');
+        }
+        else
+        {
+            /* graphic character */
+            pos = inc(pos,1);
+            putchar(c);
+        }
+    return 0;
+}
+
+/* inc : increment position counter for output */
+int inc(int pos,int n)
+{
+    if(pos + n < MAXLINE)
+        return pos + n;
+    else
+    {
+        putchar('\n');
+        return n;
+    }
+}
--- tutorial/dennis_ritchie_c/07_input_and_output/ex7-09_isupper.c
+++ tutorial/dennis_ritchie_c/07_input_and_output/ex7-09_isupper.c
@@ -2,5 +2,36 @@
 
    Exercise 7-9: Functions like isupper can be implemented to save space or to
                  save time. Explore both possibilities.
+
+   http://www.learntosolveit.com/cprogramming/Ex_7.9.html
 */
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int myisupper(int);
+
+int main(void) {
+    int c;
+
+    while ((c = getchar()) != 'x')
+    {
+        if (c == '\n')
+            continue;
+
+        if (myisupper(c) == 1)
+            printf("true\n");
+        else
+            printf("false\n");
+    }
+    return EXIT_SUCCESS;
+}
+
+int myisupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return 1;
+    else
+        return 0;
+}
