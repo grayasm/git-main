a0301c0a18318e8ddb258ef2215f80035a5999f7
Mihai Vasilian <grayasm@gmail.com>
2019-02-17 17:44:27 +0100

    improve GRANT and REVOKE example

---
+++ tutorial/postgresql_11.1_doc/05_data_definitions/23_schemas_and_privileges.sql
@@ -0,0 +1,11 @@
+/*  Chapter 5. Data Definition
+    5.8. Schemas
+    5.8.4. Schemas and Privileges
+
+    psql -d mytestdb -U pgmihai
+    \i 23_schemas_and_privileges.sql
+    \?
+    \qecho '\033[2J'
+*/
+
+
