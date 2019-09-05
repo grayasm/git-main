b9f34b4c146847fe8d3300b25f3de533920324f3
Mihai Vasilian <grayasm@gmail.com>
2019-09-05 14:00:39 +0200

    move autocritical_section to sys library

---
+++ lib/sys/src/autocritical_section.hpp
@@ -0,0 +1,51 @@
+/*
+  Copyright (C) 2014 Mihai Vasilian
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program. If not, see http://www.gnu.org/licenses/.
+
+  contact: grayasm@gmail.com
+*/
+
+
+
+#ifndef __autocritical_section_hpp__
+#define __autocritical_section_hpp__
+
+#include "autoptr.hpp"
+#include "critical_section.hpp"
+
+namespace misc
+{
+	class autocritical_section
+	{
+	public:
+		typedef misc::autoptr<autocritical_section> Ptr;
+
+	public:
+		autocritical_section(critical_section& cs);
+		~autocritical_section();
+
+	private:
+		//! non copy-able
+		autocritical_section(const autocritical_section&);
+		autocritical_section& operator=(const autocritical_section&);
+
+	private:
+		critical_section& m_critical_section;
+	};
+} // namespace
+
+
+#endif // __autocritical_section_hpp__
+
