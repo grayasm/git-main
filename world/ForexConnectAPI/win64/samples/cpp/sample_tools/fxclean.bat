9274b92c46537182c74325a66eb6444c8de633ae
Mihai Vasilian <grayasm@gmail.com>
2019-09-06 06:12:20 +0200

    removed api 1.4

--- world/ForexConnectAPI/win64/samples/cpp/sample_tools/fxclean.bat
+++
@@ -1,8 +0,0 @@
-@echo ****************************************************************
-@echo ------------ Start Cleaning sample_tools -----------------------
-
-set DOC_CPP="doc\cpp"
-
-@if exist bin rmdir bin /q /s
-@if exist lib rmdir lib /q /s
-@if exist obj rmdir obj /q /s
