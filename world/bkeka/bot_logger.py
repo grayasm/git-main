126f94a761744946a7279c569316c2d408dc41c9
Mihai Vasilian <grayasm@gmail.com>
2020-07-12 17:20:43 +0200

    add logger disable_logging

--- world/bkeka/bot_logger.py
+++ world/bkeka/bot_logger.py
@@ -7,30 +7,25 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(__name__))
 DEFAULT_LOG_DIR_PATH = os.path.join(SCRIPT_DIR, DEFAULT_LOG_DIRECTORY)
 
 
-def get_logger(name, log_file, disable_logging):
+def get_logger(name, log_file):
     """Function setup as many loggers as you want"""
-    logger = None
-    if disable_logging is True:
-        log_file_path = os.path.join(DEFAULT_LOG_DIR_PATH, "disabled.log")
-        handler = logging.FileHandler(log_file_path)
-        logger = logging.getLogger(name)
-        logger.setLevel(logging.INFO)
-        logger.addHandler(handler)
-        logging.disable(logging.INFO)
-    else:
-        log_file_path = os.path.join(DEFAULT_LOG_DIR_PATH, log_file)
-        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(module)s: %(message)s',
-                                      datefmt='%m/%d/%Y %H:%M:%S')
-        handler = logging.FileHandler(log_file_path)
-        handler.setFormatter(formatter)
-        logger = logging.getLogger(name)
-        logger.setLevel(logging.INFO)
-        logger.addHandler(handler)
+    log_file_path = os.path.join(DEFAULT_LOG_DIR_PATH, log_file)
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(module)s: %(message)s',
+                                  datefmt='%m/%d/%Y %H:%M:%S')
+    handler = logging.FileHandler(log_file_path)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
     return logger
 
 
-def close_logger(logger):
+def close_logger(logger, disable_logging):
     handlers = logger.handlers[:]
     for handler in handlers:
+        filename = handler.baseFilename
         handler.close()
         logger.removeHandler(handler)
+        # Remove file if logging is disabled
+        if disable_logging is True and filename is not None and os.path.exists(filename):
+            os.remove(filename)
