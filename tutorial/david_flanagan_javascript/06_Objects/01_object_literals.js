7deaae71bcba8e4a1d35507835e5b67b0113d1ab
Mihai Vasilian <grayasm@gmail.com>
2021-10-31 17:06:45 +0100

    add samples

--- tutorial/david_flanagan_javascript/06_Objects/01_object_literals.js
+++ tutorial/david_flanagan_javascript/06_Objects/01_object_literals.js
@@ -1,5 +1,54 @@
 /* Cap.6 Objects
-   6.
+   The Object:
+       > is the JavaScript's fundamental datatype,
+       > is an unordered collection of properties, each with a name and a value
+       > property name is a string which maps to a value
+       > property name may be any string, including the empty string
+       > string-to-value mapping is also known as: hash, hashtable, dictionary
+       > cannot have two properties with the same name
+       > property value may be any value or may be a getter or a setter function
+         or both.
 
-   $ node __node__
+       > inherits the properties of another object, known as its "prototype"
+       > is dynamic, as properties can be added and deleted
+       > is everything else that is not a string, a number, true, false, null or undefined
+       > is mutable and manipulated by reference, not by value,
+         var y = x; holds a reference and modifications to y are visibile to x;
+
+       > property attributes are writable, enumerable, configurable:
+         >> writable    : whether the value of the property can be set
+         >> enumerable  : whether the property name is returned by a for/in loop
+         >> configurable: whether the property can be deleted and its attributes can be altered
+
+   The Object:
+       > has 3 associated object attributes:
+         >> prototype       : is a reference to another object from which properties are inherited
+         >> class           : is a string that categorizes the type of an object
+         >> extensible flags: whether new properties may be added to the object
+
+   The Object classification:
+       > native            : defined by ECMAScript (e.g. arrays, dates, regexp, etc)
+       > host              : defined by the host environment-browser (e.g. HTMLElement object)
+       > user-defined      : any object created by the execution of JavaScript code
+       > own-property      : a property defined directoy on an object
+       > inherited-property: a property defined by an object's prototype object
+
+   6.1.1 Object Literals
+
+   $ node 01_object_literals.js
+
 */
+
+var empty = {};                                 // An object with no properties
+var point = { x:0, y:0};                        // Two properties
+var point2 = { x:point.x, y:point.y+1 };        // More complex values
+
+var book = {
+    "main title": "JavaScript",                 // Property names include spaces
+    'sub-title': "The Definitive Guide",        // and hyphens, so use string literals
+    "for": "all audiences",                     // for is a reserved word, so quote
+    author: {                                   // The value of this property is
+        firstname: "David",                     // itself and object. Note that
+        surname: "Flanagan"                     // these property names are unquoted.
+    }
+};
--- tutorial/david_flanagan_javascript/06_Objects/02_object_new.js
+++ tutorial/david_flanagan_javascript/06_Objects/02_object_new.js
@@ -1,5 +1,16 @@
 /* Cap.6 Objects
-   6.
+   6.1.2 Creating Objects with new
 
-   $ node __node__
+   > 'new' operator creates and initializes a new object
+   > 'new' keyword is followed by a function called constructor
+   > JavaScript includes buit-in constructors for native types
+
+
+   $ node 02_object_new.js
 */
+
+
+var o = new Object();       // create an empty object: same as {}
+var a = new Array();        // create an empty array: same as []
+var d = new Date();         // create a Date object repr the current time
+var r = new RegExp("js");   // create a RegExp object for pattern matching
