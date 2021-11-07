8112349d5a5bae4dd04e848c0e105ecc3e2c2078
Mihai Vasilian <grayasm@gmail.com>
2021-11-07 08:16:45 +0100

    adjust for dnf

--- script/gtk/install-gtk2-devel.sh
+++ script/gtk/install-gtk2-devel.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 if [ ! -f /etc/centos-release ]; then
-	echo "only for centos"
-	exit
+    echo "only for centos"
+    exit
 fi
 
 if [ `id -u` -ne 0 ]; then
-	echo "required: root"
-	exit
+    echo "required: root"
+    exit
 fi
 
 
-yum provides "/usr/include/gtk-2.0/gtk/gtk.h"
-yum install gtk2-devel
+dnf provides "/usr/include/gtk-2.0/gtk/gtk.h"
+dnf install gtk2-devel
 # gcc ... `pkg-config --cflags --libs gtk+-2.0`
 
 
-yum provides "/usr/include/libglade-2.0/glade/glade.h"
-yum install libglade2-devel
+dnf provides "/usr/include/libglade-2.0/glade/glade.h"
+dnf install libglade2-devel
 # gcc ... `pkg-config --cflags --libs gtk+-2.0 libglade-2.0`
 
 
 #User Interface Designer for GTK+ 2
-yum install glade3
+dnf install glade3
