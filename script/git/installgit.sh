1f4f34a6691459fcc1209d7ea8128ca373871de5
Mihai Vasilian <grayasm@gmail.com>
2021-11-07 08:13:36 +0100

    text formatting

--- script/git/installgit.sh
+++ script/git/installgit.sh
@@ -4,14 +4,14 @@
 git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 if [ $GIT_IS_AVAILABLE -ne 0 ]; then
-	echo "install git"
-	exit
+    echo "install git"
+    exit
 fi
 
 # need meld from Epel/Fedora (see: https://meldmerge.org/)
 if [ ! -f /usr/bin/meld ]; then
-	echo "install meld from epel or fedora"
-	exit
+    echo "install meld from epel or fedora"
+    exit
 fi
 
 # git calls external diff with 7 parameters
@@ -21,16 +21,16 @@ chmod 755 $HOME/.git-diff-wrapper.sh
 
 # configure external diff command
 if [ ! -f $HOME/.gitconfig ]; then
-	echo "create $HOME/.gitconfig"
-	touch $HOME/.gitconfig
+    echo "create $HOME/.gitconfig"
+    touch $HOME/.gitconfig
 fi
 
 
 grep -n '[diff]' $HOME/.gitconfig
 if [ $? -eq 0 ]; then
-	echo "$HOME/.gitconfig has [diff] section already; check it;"
-	emacs $HOME/.gitconfig &
+    echo "$HOME/.gitconfig has [diff] section already; check it;"
+    emacs $HOME/.gitconfig &
 else
-	printf '\n%s'   '[diff]' >> $HOME/.gitconfig
-	printf '\n\t%s' 'external = ~/.git-diff-wrapper.sh' >> $HOME/.gitconfig
+    printf '\n%s'   '[diff]' >> $HOME/.gitconfig
+    printf '\n\t%s' 'external = ~/.git-diff-wrapper.sh' >> $HOME/.gitconfig
 fi
