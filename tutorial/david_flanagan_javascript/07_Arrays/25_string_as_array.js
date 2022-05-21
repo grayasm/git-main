3d45f597caec7c9a796644f203475f0297474710
Mihai Vasilian <grayasm@gmail.com>
2022-05-21 14:40:39 +0200

    strings as array

--- tutorial/david_flanagan_javascript/07_Arrays/25_string_as_array.js
+++ tutorial/david_flanagan_javascript/07_Arrays/25_string_as_array.js
@@ -12,8 +12,9 @@ s[1];        // => "e"
 
 
 var s = "JavaScript";
-Array.prototype.join.call(s, " ");    // => J a v a S c r i p t
-Array.prototype.filter.call(s,
-                            function(x) {
-                                return x.match(/[^aeiou]/); // match nonvowels
-                            }).join("");                    // JvScrpt
+Array.prototype.join.call(s, " ");  // => J a v a S c r i p t
+Array.prototype.filter.call(
+    s,                              // Filter the characters of the s
+    function(x) {
+        return x.match(/[^aeiou]/); // Only match nonvowels
+    }).join("");                    // => "JvScrpt"
