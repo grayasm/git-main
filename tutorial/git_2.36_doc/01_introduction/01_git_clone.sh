dad2acfe1fe98023d4f9926467d9468ee117cece
Mihai Vasilian <grayasm@gmail.com>
2022-06-17 16:49:01 +0200

    add introduction

---
+++ tutorial/git_2.36_doc/01_introduction/01_git_clone.sh
@@ -0,0 +1,12 @@
+#
+# https://git-scm.com/docs/user-manual#_introduction
+#
+# Comprehensive reference documentation is available through the man pages
+#
+# man git-clone
+# git help clone
+# 
+
+echo "Use the manual to get help (e.g. on \"clone\" command)"
+echo "man git-clone"
+echo "git help clone"
