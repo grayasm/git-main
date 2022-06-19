faf9546c13553753b7802216fcedcdb899719dc8
Mihai Vasilian <grayasm@gmail.com>
2022-06-19 19:41:53 +0200

    git branch -r

---
+++ tutorial/git_2.36_doc/02_repos_and_branches/11_git_branch_remotes.sh
@@ -0,0 +1,22 @@
+# https://git-scm.com/docs/user-manual#examining-remote-branches
+#
+# You can view the remote branches using: git branch -r
+#
+# man git-branch
+
+
+if [ ! -d git-playground ]; then
+	git clone https://github.com/grayasm/git-playground.git
+fi
+
+cd git-playground
+
+printf "\nList all remote branches with: git branch -r\n"
+
+git branch -r
+
+#  origin/HEAD -> origin/main
+#  origin/main
+#  origin/v.2022-06-19_1015
+#  origin/v.2022-06-19_1040
+#  origin/v.2022-06-19_1720
