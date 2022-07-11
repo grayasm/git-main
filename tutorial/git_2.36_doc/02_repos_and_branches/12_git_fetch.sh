76434d856b0d1f4e0701dcadf4d1e5ae466a69e1
Mihai Vasilian <grayasm@gmail.com>
2022-07-11 13:16:59 +0200

    add git-fetch and git-remote

---
+++ tutorial/git_2.36_doc/02_repos_and_branches/12_git_fetch.sh
@@ -0,0 +1,25 @@
+# https://git-scm.com/docs/user-manual#Updating-a-repository-With-git-fetch
+#
+# The git-fetch command, with no arguments, will update all of the 
+# remote tracking branches to the latest version found in the original
+# repository. It will not touch any of your own branches - not even the
+# "master" branch that was created for you on clone.
+#
+# man git-fetch
+
+if [ ! -d git-playground ]; then
+	git clone https://github.com/grayasm/git-playground.git
+fi
+
+cd git-playground
+
+# prepare repository first (clean up)
+git switch main
+
+
+# update
+git fetch origin
+
+
+# to see the repo names that can be fetched use
+git remote -v
---
+++ tutorial/git_2.36_doc/02_repos_and_branches/13_git_remote_add.sh
@@ -0,0 +1,27 @@
+# https://git-scm.com/docs/user-manual#fetching-branches
+#
+# You can also track branches from repositories other than the one you cloned
+# from, using git-remote
+#
+# man git-remote
+
+if [ ! -d git-playground ]; then
+	git clone https://github.com/grayasm/git-playground.git
+fi
+
+cd git-playground
+
+# prepare repository first (clean up)
+git switch main
+
+# remove git-forkground.git if added previously
+git remote remove staging
+
+# track branches from repositories other than the one you cloned from
+git remote add staging https://github.com/grayasm/git-forkground.git
+
+# list remote branches
+git branch -r
+
+# update the remote-tracking branches
+git fetch staging
