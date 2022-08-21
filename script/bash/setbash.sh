70d6cfdb80bc6c02435829eed4b6e692437cd005
mihai <mihai@binary.fritz.box>
2022-08-21 12:58:47 +0200

    copy to ~/.bashrc.d

--- script/bash/setbash.sh
+++ script/bash/setbash.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# verbose
-set -x
-
 # check location
 if [ "${PWD##*/}" != "bash" ]; then
     echo "switch to script directory!"
     exit
 fi
 
-# copy files
-cp -v $HOME/.bashrc $HOME/.bashrc.old
-cp -v bashrc        $HOME/.bashrc
-cp -v bash_aliases  $HOME/.bash_aliases
-cp -v bash_nosave   $HOME/.bash_nosave
-cp -v bash_i3       $HOME/.bash_i3
-cp -v bash_gpg      $HOME/.bash_gpg
-cp -v bash_iptables $HOME/.bash_iptables
+[ -d $HOME/.bashrc.d ] || mkdir -v $HOME/.bashrc.d
+
+cp -v bash_aliases  $HOME/.bashrc.d/bash_aliases
+cp -v bash_nosave   $HOME/.bashrc.d/bash_nosave
+cp -v bash_i3       $HOME/.bashrc.d/bash_i3
+cp -v bash_gpg      $HOME/.bashrc.d/bash_gpg
+cp -v bash_iptables $HOME/.bashrc.d/bash_iptables
