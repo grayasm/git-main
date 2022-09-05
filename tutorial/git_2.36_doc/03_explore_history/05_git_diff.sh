b3c22cd4798452413fe17fb9fc56b2339a4f0da6
Mihai Vasilian <grayasm@gmail.com>
2022-09-05 09:13:43 +0200

    add git-format-patch

---
+++ tutorial/git_2.36_doc/03_explore_history/05_git_diff.sh
@@ -0,0 +1,39 @@
+# https://git-scm.com/docs/user-manual#generating-diffs
+#
+# Generate diffs between any two revision using git-diff
+#
+# man git-diff
+# man git-format-patch
+
+
+if [ ! -d git-playground ]; then
+    git clone  https://github.com/grayasm/git-playground.git
+    cd git-playground
+    git remote add staging https://github.com/grayasm/git-forkground.git
+    git fetch staging
+    cd ..
+fi
+
+cd git-playground
+
+# switch to main before clean up
+git switch main
+
+# delete --force local branch first
+git branch -D v.2022-08-24_1440
+
+# checkout clean
+git switch -c v.2022-08-24_1440 staging/v.2022-08-24_1440
+
+# git diff 2 branches
+git diff staging/main..staging/v.2022-08-24_1440
+
+# generate instead a set of patches using git-format-patch
+git format-patch staging/main..HEAD
+
+# patch files should not be commited
+echo -e "\n!!!!! DO NOT COMMIT THESE .patch FILES !!!!!\n"
+git status .
+
+# clean up
+rm -v 00*.patch
