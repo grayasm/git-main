d89889f5dc09d11f4ad98db979976d9df2baa8ad
Mihai Vasilian <grayasm@gmail.com>
2023-02-07 12:01:48 +0100

    add LICENSE.txt and modify files header

--- lib/fxcmAdapter/src/PipCost.cpp
+++ lib/fxcmAdapter/src/PipCost.cpp
@@ -1,21 +1,8 @@
 /*
 	Copyright (C) 2014 Mihai Vasilian
+*/
 
-	This program is free software; you can redistribute it and/or modify it
-	under the terms of the GNU General Public License as published by the
-	Free Software Foundation; either version 2 of the License, or (at your
-	option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-	See the GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License along
-	with this program. If not, see http://www.gnu.org/licenses/.
-
-	contact: grayasm@gmail.com
-
+/*
 	resources:
 	www.forexnewbies.com/pip-value-formula
 	www.fxtrade.oanda.com/analysis/profit-calculator
--- lib/fxcmAdapter/src/TradingPermissions.cpp
+++ lib/fxcmAdapter/src/TradingPermissions.cpp
@@ -1,4 +1,6 @@
-
+/*
+Copyright (C) 2017 Mihai Vasilian
+*/
 
 
 #include "TradingPermissions.hpp"
