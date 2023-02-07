d7de98bbdda86b112f351cb389463672cf8d1f46
Mihai Vasilian <grayasm@gmail.com>
2023-02-07 15:14:54 +0100

    add LICENSE.txt and modify files header

--- lib/sys/src/unistd.hpp
+++ lib/sys/src/unistd.hpp
@@ -1,20 +1,5 @@
 /*
-  Copyright (C) 2013 Mihai Vasilian
-
-  This program is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published by the
-  Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License along
-  with this program. If not, see http://www.gnu.org/licenses/.
-
-  contact: grayasm@gmail.com
+Copyright (C) 2013 Mihai Vasilian
 */
 
 
@@ -80,14 +65,16 @@ typedef unsigned short     uint16_t;
 typedef unsigned int       uint32_t;
 typedef unsigned long long uint64_t;
 
-/*typedef __int8            int8_t;
+/*
+typedef __int8            int8_t;
 typedef __int16           int16_t; 
 typedef __int32           int32_t;
 typedef __int64           int64_t;
 typedef unsigned __int8   uint8_t;
 typedef unsigned __int16  uint16_t;
 typedef unsigned __int32  uint32_t;
-typedef unsigned __int64  uint64_t;*/
+typedef unsigned __int64  uint64_t;
+*/
 
 #endif // _WIN32
 
