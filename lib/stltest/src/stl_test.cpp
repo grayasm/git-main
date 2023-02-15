8c69017ffca220208e6520153d8534f94763e6d9
Mihai Vasilian <grayasm@gmail.com>
2023-02-15 08:51:25 +0100

    remove tests that are not implemented; to be done later if needed

--- lib/stltest/src/stl_test.cpp
+++ lib/stltest/src/stl_test.cpp
@@ -9,39 +9,25 @@ Copyright (C) 2012 Mihai Vasilian
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
-/*
+
 #include "test_algorithm.hpp"
 #include "test_autoptr.hpp"
-#include "test_critical_section.hpp"
-#include "test_autocritical_section.hpp"
 #include "test_deque.hpp"
-#include "test_event.hpp"
 #include "test_exception.hpp"
-#include "test_filename.hpp"
 #include "test_functional.hpp"
 #include "test_generic_array.hpp"
 #include "test_list.hpp"
-#include "test_multi_lock.hpp"
-#include "test_mutex.hpp"
 #include "test_numeric.hpp"
 #include "test_generic_string.hpp"
 #include "test_vector.hpp"
 #include "test_priority_queue.hpp"
 #include "test_queue.hpp"
-#include "test_semaphore.hpp"
-#include "test_single_lock.hpp"
 #include "test_stack.hpp"
-#include "test_thread.hpp"
-#include "test_tree.hpp"
 #include "test_utility.hpp"
-*/
 #include "test_basic_string.hpp"
 #include "test_vector.hpp"
 #include "test_list.hpp"
 
-/*
-#include "test_time.hpp"
-*/
 
 
 int main(int argc, char** argv)
@@ -51,45 +37,21 @@ int main(int argc, char** argv)
 
     //http://cppunit.sourceforge.net/doc/lastest/cppunit_cookbook.html
 
-    //////////////////////////////////////////////////////////////////////////
-    //algorithms
-    /*
-      algorithm
-      functional
-      math
-      memmanip
-      memory
-      numeric
-      utility
-    */
-
-#if 0
+#if 1
     CPPUNIT_TEST_SUITE_REGISTRATION(test_algorithm);
+    CPPUNIT_TEST_SUITE_REGISTRATION(test_autoptr);
+    CPPUNIT_TEST_SUITE_REGISTRATION(test_basic_string);
+    CPPUNIT_TEST_SUITE_REGISTRATION(test_deque);
+    CPPUNIT_TEST_SUITE_REGISTRATION(test_exception);
     CPPUNIT_TEST_SUITE_REGISTRATION(test_functional);
+
     CPPUNIT_TEST_SUITE_REGISTRATION(test_numeric);
     CPPUNIT_TEST_SUITE_REGISTRATION(test_utility);
 #endif
 
 
-    //////////////////////////////////////////////////////////////////////////
-    //containers
-    /*
-      autoptr
-      exception
-      iterator
-      generic_array
-      vector
-      string
-      list
-      stack
-      deque
-      queue
-      priority_queue
-      tree
-    */
 
-
-    CPPUNIT_TEST_SUITE_REGISTRATION(test_basic_string);
+    
     CPPUNIT_TEST_SUITE_REGISTRATION(test_vector);
     CPPUNIT_TEST_SUITE_REGISTRATION(test_list);
 
