4335c141bd020bb6d905624442af8782de2029c3
Mihai Vasilian <grayasm@gmail.com>
2023-05-26 14:07:12 +0200

    use TypeConverter in procedural code

--- tutorial/adam_nathan_WPF4.5/02/13_TypeConverter/MainWindow.xaml.cs
+++ tutorial/adam_nathan_WPF4.5/02/13_TypeConverter/MainWindow.xaml.cs
@@ -23,6 +23,29 @@ namespace TypeConverter
         public MainWindow()
         {
             InitializeComponent();
+
+
+            // Create a Brush using a TypeConverter
+            System.ComponentModel.TypeConverter tc = System.ComponentModel.TypeDescriptor.GetConverter(typeof(Brush));
+            Brush br = (Brush)tc.ConvertFromInvariantString("White");
+
+            // Grab the OK button, knowing only its name
+            Button b = (Button)this.FindName("okButton");
+            b.Background = br;
+
+            // Crate a Brush differently
+            System.Windows.Shapes.Rectangle r = new System.Windows.Shapes.Rectangle();
+            r.Width = 40;
+            r.Height = 40;
+            r.Fill = System.Windows.Media.Brushes.Black;
+            b.Content = r;
+
+
+            // Display the Window
+            this.Show();
+
+            // Show the Button's Content
+            MessageBox.Show(okButton.Content.ToString());
         }
     }
 }
