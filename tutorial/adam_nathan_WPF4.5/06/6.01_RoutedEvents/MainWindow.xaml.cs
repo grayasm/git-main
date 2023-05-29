36348cd31177b8d0ac33e814537508e3d7ef9369
Mihai Vasilian <grayasm@gmail.com>
2023-05-29 17:25:39 +0200

    finish RoutedEvents sample

--- tutorial/adam_nathan_WPF4.5/06/6.01_RoutedEvents/MainWindow.xaml.cs
+++ tutorial/adam_nathan_WPF4.5/06/6.01_RoutedEvents/MainWindow.xaml.cs
@@ -24,5 +24,23 @@ namespace RoutedEvents
         {
             InitializeComponent();
         }
+
+        void MainWindow_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Display information about this event
+            this.Title = "Source = " + e.Source.GetType().Name + ", OriginalSource = " +
+            e.OriginalSource.GetType().Name + " @ " + e.Timestamp;
+            // In this example, all possible sources derive from Control
+            Control source = e.Source as Control;
+            // Toggle the border on the source control
+            if (source.BorderThickness != new Thickness(5))
+            {
+                source.BorderThickness = new Thickness(5);
+                source.BorderBrush = Brushes.Black;
+            }
+            else
+                source.BorderThickness = new Thickness(0);
+        }
+
     }
 }
