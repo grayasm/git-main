f840603e000fd52330effffa91e4f170ba2974f5
Mihai Vasilian <grayasm@gmail.com>
2023-11-22 17:29:57 +0100

    git merge doc

--- tutorial/git_2.36_doc/04_developing_with_git/06_git_merge.sh
+++ tutorial/git_2.36_doc/04_developing_with_git/06_git_merge.sh
@@ -1,26 +1,33 @@
 # https://git-scm.com/docs/user-manual#how-to-merge
 #
-# Continue from here.....
-
-
+# git merge <branchname>     -- rejoin two diverging branches of development
 
 
 if [ ! -d git-playground ]; then
-	git clone  https://github.com/grayasm/git-playground.git
-	cd git-playground
-	git remote add staging https://github.com/grayasm/git-forkground.git
-	git fetch staging
-	cd ..
+    git clone  https://github.com/grayasm/git-playground.git
+    cd git-playground
+    git remote add staging https://github.com/grayasm/git-forkground.git
+    git fetch staging
+    cd ..
 fi
 
 cd git-playground
 
-# switch to main before clean up
+# clean up before actual work
 git switch main
+git checkout .
+git branch -D v.2023-05-18_0934_temp
+git branch -D v.2023-05-18_0934
+git fetch staging
 
-# delete --force local branch first
-git branch -D v.2022-07-11_1520
+# switch to main at 2023-05-18 09:30:31
+git switch --detach 866ce5e4afaefbb71b712647dac62d5bcc96b092
 
-# checkout clean
-git switch -c v.2022-07-11_1520 staging/v.2022-07-11_1520
+# create a branch at current revision
+git checkout -b v.2023-05-18_0934_temp
 
+# merge a branch into current
+git merge staging/v.2023-05-18_0934
+
+# last 3 commits are relevant
+git log -n 3
--- tutorial/git_2.36_doc/04_developing_with_git/07_resolve_a_merge.sh
+++ tutorial/git_2.36_doc/04_developing_with_git/07_resolve_a_merge.sh
@@ -1,3 +1,19 @@
 # https://git-scm.com/docs/user-manual#resolving-a-merge
 #
 #
+
+
+if [ ! -d git-playground ]; then
+    git clone  https://github.com/grayasm/git-playground.git
+    cd git-playground
+    git remote add staging https://github.com/grayasm/git-forkground.git
+    git fetch staging
+    cd ..
+fi
+
+cd git-playground
+
+# clean up before actual work
+git switch main
+git checkout .
+
