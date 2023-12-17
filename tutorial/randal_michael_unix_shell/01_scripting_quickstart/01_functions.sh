2335ae5e0dce1da05bed0feea349be6339d463d5
Mihai Vasilian <grayasm@gmail.com>
2023-12-17 23:55:32 +0100

    add function

---
+++ tutorial/randal_michael_unix_shell/01_scripting_quickstart/01_functions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# A function has one of the following 2 forms:
+
+function function_name
+{
+    echo "some commands"
+}
+
+another_function()
+{
+    echo "another commands"
+}
+
+function_name
+another_function
