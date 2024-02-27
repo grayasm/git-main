b8a2c4c8d7ab228db131d8c9b5ee2e5ffbf4b318
Mihai Vasilian <grayasm@gmail.com>
2024-02-27 10:38:50 +0100

    add at example

--- tutorial/randal_michael_unix_shell/01_scripting_quickstart/26_at.sh
+++ tutorial/randal_michael_unix_shell/01_scripting_quickstart/26_at.sh
@@ -8,40 +8,59 @@
 # systemctl status atd
 
 
-# 1) Schedule a job using 'at' command prompt:
+# [[ Quick example: ]]
+
+# echo `date` > /tmp/log.txt | at 16:45     <-- run the command at 16:45 and
+#                                               then delete it.
+# at -l                                     <-- list all current pending jobs
+# at -r 15                                  <-- remove task number 15
+
+
+# [[ Multiple ways to schedule a one-time job: ]]
+
+# Using 'at' command prompt:
 # at 16:45                        <--and press enter
 # echo `date` > /tmp/log.txt
 #                                 <-- Ctrl+D to exit
 # The job will run at 16:45 and then be deleted.
 
 
-# 2) Schedule a job using echo and pipe | to at:
+# Using echo and | :
 # echo `date` > /tmp/log.txt | at 16:45
 
 
-# 3) Schedule a job using HERE document:
+# Using HERE document:
 # at 16:45 <<EOF
 # echo `date` > /tmp/log.txt
 # EOF
 
 
-# 4) Schedule a job by passing the -f script directly:
+# Using the -f:
 # at 16:45 -f /scripts/script.sh
 
 
-# 5) Use -M to avoid sending and email with the output to the user:
+# [[ Options: ]]
+
+# Use -M to avoid sending and email with the output to the user:
 # at 16:45 -M
 
 
-# 6) Use -m to send an email even if there is no output:
+# Use -m to send an email even if there is no output:
 # at 16:45 -m
 
 
-# 7) Use at -b or batch to run a command when system load average is below 1.5
+# Use at -b or batch to run a command when system load average is below 1.5
 # echo `date` > /tmp/log.txt | batch
 
 
-# at [OPTIONS] time_specifications
+# Use -q to specify to which queue to add the job.
+# The queues are a to z and A to Z. Lower letters run with lower niceness.
+# By default at adds the jobs to a queue, and batch to b queue.
+
+# at monday +2 hours -q L
+
+
+# [[ Time specifications: ]]
 
 # 'at' allows pretty complex time specifications with TIME, DATE and INCREMENT
 # in combinations.
@@ -65,10 +84,10 @@
 # time-unit can be: minutes, hours, days, weeks
 
 
-# Examples, using 'at' command prompt.
+# Examples:
 
-# at sunday +10 minutes
-# if time now is 18:00:00 it will schedule a job for next sunday at 18:10:00
+# at sunday +10 minutes    <-- if time now is 18:00:00
+#                              the job will run next sunday at 18:10:00
 
 # at 1pm +2 days           <-- at 1pm two days from now
 
@@ -77,3 +96,20 @@
 # at now +1 hours          <-- one hour from now
 
 # at -t 202402261810.30    <-- use -t to specify [[CC]YY]MMDDhhmm[.ss]
+
+
+
+# [[ Miscellaneous: ]]
+
+# at -l                    <-- list pending jobs, similar with 'atq'
+
+# at -r 9                  <-- remove pending job 9, similar with atrm 9
+
+
+# /etc/at.deny             <-- if exists and empty all users can use 'at'
+#                              if not empty, listed users cannot use 'at'
+
+# /etc/at.allow            <-- if exists, only listed users can use 'at'
+
+# If neither of the files exists, only the users with administrative privileges
+# can use the 'at' command.
